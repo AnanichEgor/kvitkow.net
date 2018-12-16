32c9436d22ac9e98e28a7c444d7b4c75e0b32577
Rodion <dreyevgo@gmail.com>
2018-12-16 22:47:41 +0300

    + add Ticket class

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Ticket/Ticket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvitkouNet.Logic.Common.Models.Ticket
+{
+    public class Ticket
+    {
+        public bool Free { get; set; }
+
+        public string Name { get; set; }
+
+        public int TicketId { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Date { get; set; }
+
+        public double Price { get; set; }
+
+        public string Properties { get; set; }
+
+        public string SellerPhone { get; set; }
+
+        public string SellerAdress { get; set; }
+
+        public string PaymentSystems { get; set; }
+
+        public string TimeActual { get; set; }
+
+        public string TypeEvent { get; set; }
+
+        public string EventLink { get; set; }
+    }
+}
