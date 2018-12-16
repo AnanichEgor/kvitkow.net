961333d4f31719b1f7c748ce0ac8aca7efd7a22b
Alexander Pashnikov <alexpvt@yandex.ru>
2018-12-16 22:29:36 +0300

    added InternalErrorLogEntry model

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Logging/InternalErrorLogEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvitkouNet.Logic.Common.Models.Logging
+{
+    public class InternalErrorLogEntry
+    {
+        public long Id { get; set; }
+        
+        public string TypeString { get; set; }
+
+        public int HResult { get; set; }
+
+        public string InnerExceptionString { get; set; }
+
+        public string Message { get; set; }
+
+        public string Source { get; set; }
+
+        public string StackTrace { get; set; }
+
+        public string TargetSiteName { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
