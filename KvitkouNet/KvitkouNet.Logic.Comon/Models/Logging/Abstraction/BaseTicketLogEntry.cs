03f1434a20ea9b1ebc2eb1aad54f1c97ea8b2049
Alexander Pashnikov <alexpvt@yandex.ru>
2018-12-16 23:02:32 +0300

    added abstract logentry models

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Logging/Abstraction/BaseTicketLogEntry.cs
@@ -0,0 +1,18 @@
+﻿namespace KvitkouNet.Logic.Common.Models.Logging.Abstraction
+{
+    /// <summary>
+    /// Базовый класс для создание доменных моделей записей лога о билетах
+    /// </summary>
+    internal abstract class BaseTicketLogEntry : BaseLogEntry<int>
+    {
+        /// <summary>
+        /// Билет
+        /// </summary>
+        public Ticket Ticket { get; set; }
+
+        /// <summary>
+        /// Пользователь-владелец билета
+        /// </summary>
+        public User Owner { get; set; }
+    }
+}
