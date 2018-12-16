27aeb3d12364da4e37de6ee2b5cc2913548c30bc
Alexander Pashnikov <alexpvt@yandex.ru>
2018-12-16 22:28:46 +0300

    added TicketActionLogEntry model

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Logging/TicketActionLogEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using KvitkouNet.Logic.Common.Enums;
+
+namespace KvitkouNet.Logic.Common.Models.Logging
+{
+    /// <summary>
+    /// Модель записи в лог о действии с билетом
+    /// </summary>
+    public class TicketActionLogEntry
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Билета, над которым совершено действие
+        /// </summary>
+        public Ticket Ticket { get; set; }
+
+        /// <summary>
+        /// Пользователь-владелец билета
+        /// </summary>
+        public User Owner { get; set; }
+
+        /// <summary>
+        /// Тип действия с билетом
+        /// </summary>
+        public TicketAction Action { get; set; }
+
+        /// <summary>
+        /// Содержимое действия
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Дата совершения действия
+        /// </summary>
+        public DateTime Created { get; set; }
+    }
+}
