add864650207e63fd5e4a391d69167a2c970f9c8
artemGlotov <artsiom.hlotau@gmail.com>
2018-12-17 23:58:21 +0300

    add notification model

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Notification/EmailNotification.cs
@@ -0,0 +1,18 @@
+﻿namespace KvitkouNet.Logic.Common.Models.Notification
+{
+	/// <summary>
+	/// Уведомление по электронной почте
+	/// </summary>
+	class EmailNotification : Notification
+	{
+		/// <summary>
+		/// Отправитель
+		/// </summary>
+		public string Sender { get; set; }
+
+		/// <summary>
+		/// Электронный ящик
+		/// </summary>
+		public string Email { get; set; }
+	}
+}
---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Notification/Notification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KvitkouNet.Logic.Common.Models.Notification
+{
+	/// <summary>
+	/// Уведомление
+	/// </summary>
+	public class Notification
+	{
+		/// <summary>
+		/// Заголовок уведомления
+		/// </summary>
+		public string Title { get; set; }
+
+		/// <summary>
+		/// Текст уведомления
+		/// </summary>
+		public string Message { get; set; }
+
+		/// <summary>
+		/// Дата отправки
+		/// </summary>
+		public DateTime Date { get; set; }
+
+		/// <summary>
+		/// Получатель
+		/// </summary>
+		public string Receiver { get; set; }
+	}
+}
