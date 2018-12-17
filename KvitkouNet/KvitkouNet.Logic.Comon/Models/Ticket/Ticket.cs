93e07949a4b8873cf7b0b013ddcc532b8017cc1b
Roma Kolyago <romakolyago18@gmail.com>
2018-12-17 11:54:19 +0300

    * fix type TypeEvent

--- KvitkouNet/KvitkouNet.Logic.Comon/Models/Ticket/Ticket.cs
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Ticket/Ticket.cs
@@ -1,81 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using KvitkouNet.Logic.Common.Models.Enums;
 
 namespace KvitkouNet.Logic.Common.Models.Ticket
 {
     /// <summary>
-    /// Класс описания доменной модели билета
+    ///     Класс описания доменной модели билета
     /// </summary>
     public class Ticket
     {
         /// <summary>
-        /// Платный/бесплатный билет
+        ///     Платный/бесплатный билет
         /// </summary>
         public bool Free { get; set; }
 
         /// <summary>
-        /// Название билета
+        ///     Название билета
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// Id билета
+        ///     Id билета
         /// </summary>
         public int TicketId { get; set; }
 
         /// <summary>
-        /// Страна проведения мероприятия
+        ///     Страна проведения мероприятия
         /// </summary>
         public string Country { get; set; }
 
         /// <summary>
-        /// Город проведения мероприятия
+        ///     Город проведения мероприятия
         /// </summary>
         public string City { get; set; }
 
         /// <summary>
-        /// Дата проведения мероприятия
+        ///     Дата проведения мероприятия
         /// </summary>
         public string Date { get; set; }
 
         /// <summary>
-        /// Цена билета
+        ///     Цена билета
         /// </summary>
         public double Price { get; set; }
 
         /// <summary>
-        /// Дополнительная информация билета
+        ///     Дополнительная информация билета
         /// </summary>
         public string AdditionalData { get; set; }
 
         /// <summary>
-        /// Уникальный номер группы
+        ///     Уникальный номер группы
         /// </summary>
         public string SellerPhone { get; set; }
 
         /// <summary>
-        /// Адрес продавца
+        ///     Адрес продавца
         /// </summary>
         public string SellerAdress { get; set; }
 
         /// <summary>
-        /// Номер продавца
+        ///     Номер продавца
         /// </summary>
         public string PaymentSystems { get; set; }
 
         /// <summary>
-        /// Время актуальности билета
+        ///     Время актуальности билета
         /// </summary>
         public string TimeActual { get; set; }
 
         /// <summary>
-        /// Тип мероприятия
+        ///     Тип мероприятия
         /// </summary>
-        public string TypeEvent { get; set; }
+        public TypeEventTicket TypeEvent { get; set; }
 
         /// <summary>
-        /// Ссылка на мероприятие
+        ///     Ссылка на мероприятие
         /// </summary>
         public string EventLink { get; set; }
     }
