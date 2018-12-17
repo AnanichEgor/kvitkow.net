eceb8e3d196d1a6b3cf30b9e62e6bbb86307eee7
Bakharev Mikhail <baharev96@list.ru>
2018-12-17 21:52:51 +0300

    + added Mock models

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Logging/Mocks/DashBoardFilterMock.cs
@@ -0,0 +1,10 @@
+﻿namespace KvitkouNet.Logic.Common.Models.Logging.Mocks
+{
+	/// <summary>
+	/// Данная модель будет замещать доменную модель DashBoardFilter (или аналогичную ей)
+	/// в домене Logging до ее создания
+	/// </summary>
+	public class DashBoardFilterMock
+	{
+	}
+}
