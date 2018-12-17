ad96a5366d22c47580bd85c347878859d2d94c25
Roma Kolyago <romakolyago18@gmail.com>
2018-12-17 11:33:37 +0300

    + add comment

--- KvitkouNet/KvitkouNet.Logic.Comon/Models/Ticket/Ticket.cs
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Ticket/Ticket.cs
@@ -6,32 +6,73 @@ namespace KvitkouNet.Logic.Common.Models.Ticket
 {
     public class Ticket
     {
+        /// <summary>
+        /// Платный/бесплатный билет
+        /// </summary>
         public bool Free { get; set; }
 
+        /// <summary>
+        /// Название билета
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Id билета
+        /// </summary>
         public int TicketId { get; set; }
 
+        /// <summary>
+        /// Страна проведения мероприятия
+        /// </summary>
         public string Country { get; set; }
 
+        /// <summary>
+        /// Город проведения мероприятия
+        /// </summary>
         public string City { get; set; }
 
+        /// <summary>
+        /// Дата проведения мероприятия
+        /// </summary>
         public string Date { get; set; }
 
+        /// <summary>
+        /// Цена билета
+        /// </summary>
         public double Price { get; set; }
 
-        public string Properties { get; set; }
+        /// <summary>
+        /// Дополнительная информация билета
+        /// </summary>
 
+        /// <summary>
+        /// Уникальный номер группы
+        /// </summary>
         public string SellerPhone { get; set; }
 
+        /// <summary>
+        /// Адрес продавца
+        /// </summary>
         public string SellerAdress { get; set; }
 
+        /// <summary>
+        /// Номер продавца
+        /// </summary>
         public string PaymentSystems { get; set; }
 
+        /// <summary>
+        /// Время актуальности билета
+        /// </summary>
         public string TimeActual { get; set; }
 
+        /// <summary>
+        /// Тип мероприятия
+        /// </summary>
         public string TypeEvent { get; set; }
 
+        /// <summary>
+        /// Ссылка на мероприятие
+        /// </summary>
         public string EventLink { get; set; }
     }
 }
