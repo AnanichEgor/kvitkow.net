017afb34919857a3286af30897af508f3e2d0180
Alexander Shaduro <belpyro@gmail.com>
2018-12-18 20:44:45 +0300

    Updated to async version

--- KvitkouNet/KvitkouNet.Web/Controllers/UserController.cs
+++ KvitkouNet/KvitkouNet.Web/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Threading.Tasks;
 using KvitkouNet.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using NSwag.Annotations;
@@ -12,8 +13,9 @@ namespace KvitkouNet.Web.Controllers
         [HttpPost, Route("register")]
         [SwaggerResponse(HttpStatusCode.NoContent, typeof(void), Description = "All OK")]
         [SwaggerResponse(HttpStatusCode.BadRequest, typeof(string), Description = "Invalid model")]
-        public IActionResult Register([FromBody]UserRegisterModel model)
+        public async Task<IActionResult> Register([FromBody]UserRegisterModel model)
         {
+            await Task.Delay(100);
             return model.Password.Equals(model.ConfirmPassword, StringComparison.OrdinalIgnoreCase) ?
                 (IActionResult) NoContent() : BadRequest("Password error");
         }
