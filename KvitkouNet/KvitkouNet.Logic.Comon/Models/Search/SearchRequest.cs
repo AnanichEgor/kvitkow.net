83b42b51792faced8eb7edc473e35c05fd5cb4e3
Valeria <valiretti@gmail.com>
2018-12-18 14:52:52 +0300

    + added documentation for all search domain models

--- KvitkouNet/KvitkouNet.Logic.Comon/Models/Search/SearchRequest.cs
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Search/SearchRequest.cs
@@ -4,9 +4,19 @@ using System.Text;
 
 namespace KvitkouNet.Logic.Common.Models.Search
 {
+    /// <summary>
+    /// Contains common information for search request.
+    /// </summary>
     public abstract class SearchRequest
     {
+        /// <summary>
+        /// Gets or sets the offset from the beginning of the result set.
+        /// </summary>
         public int Offset { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximal number of items to return in the response.
+        /// </summary>
         public int Limit { get; set; }
     }
 }
