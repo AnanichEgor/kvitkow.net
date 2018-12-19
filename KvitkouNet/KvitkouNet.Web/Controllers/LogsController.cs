395ddc9497963786804ca0885773586bfbdf6e2b
Alexander Pashnikov <alexpvt@yandex.ru>
2018-12-19 21:11:02 +0300

    fixed summaries

--- KvitkouNet/KvitkouNet.Web/Controllers/LogsController.cs
+++ KvitkouNet/KvitkouNet.Web/Controllers/LogsController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using KvitkouNet.Logic.Common.Dtos.Logging;
 using KvitkouNet.Logic.Common.Models.Logging;
-using KvitkouNet.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using NSwag.Annotations;
 
@@ -20,7 +17,7 @@ namespace KvitkouNet.Web.Controllers
         /// <summary>
         /// Получает логи действий с аккаунтами пользователей
         /// </summary>
-        /// <param name="filter">Фильтр</param>
+        /// <param name="filter">Фильтр логов действий с аккаунтами</param>
         /// <returns></returns>
         [HttpGet]
         [Route("accounts")]
@@ -28,28 +25,35 @@ namespace KvitkouNet.Web.Controllers
         [SwaggerResponse(HttpStatusCode.BadRequest, typeof(string), Description = "Invalid filter")]
         public async Task<IActionResult> GetAccountLogs([FromQuery] AccountLogsFilterDto filter)
         {
-            await Task.Delay(1000);
-
+            // имитируем некоторую валидацию
             if (string.IsNullOrWhiteSpace(filter.UserName))
             {
                 return BadRequest($"Invalid filter! Property {nameof(AccountLogsFilterDto.UserName)} is empty or whitespace!");
             }
 
+            await Task.Delay(1000);
+
             return Ok(new List<AccountLogEntry>());
         }
 
+        /// <summary>
+        /// Получает логи ошибок сервера
+        /// </summary>
+        /// <param name="filter">Фильтр логов ошибок сервера</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("errors")]
         [SwaggerResponse(HttpStatusCode.OK, typeof(IEnumerable<InternalErrorLogEntry>), Description = "Error logs")]
         [SwaggerResponse(HttpStatusCode.BadRequest, typeof(string), Description = "Invalid filter")]
         public async Task<IActionResult> GetErrorLogs([FromQuery] ErrorLogsFilterDto filter)
         {
-            await Task.Delay(1000);
-
+            // имитируем некоторую валидацию
             if (string.IsNullOrWhiteSpace(filter.ExceptionTypeName))
             {
                 return BadRequest($"Invalid filter! Property {nameof(ErrorLogsFilterDto.ExceptionTypeName)} is empty or whitespace!");
             }
+
+            await Task.Delay(1000);
 
             return Ok(new List<InternalErrorLogEntry>());
         }
