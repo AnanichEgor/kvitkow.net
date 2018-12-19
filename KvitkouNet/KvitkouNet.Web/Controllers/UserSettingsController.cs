a9381db370a0f55805676ead68b23dc2b9b61ceb
Marina <for-marina@mail.ru>
2018-12-19 23:59:25 +0300

    add userSettingsController and changeNameModel

---
+++ KvitkouNet/KvitkouNet.Web/Controllers/UserSettingsController.cs
@@ -0,0 +1,48 @@
+﻿using KvitkouNet.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace KvitkouNet.Web.Controllers
+{
+	[Route("api/{id}/usersettings")]
+	public class UserSettingsController : Controller
+	{
+		[HttpPut, Route("changename/first")]
+		[SwaggerResponse(HttpStatusCode.NoContent, typeof(void), Description = "All OK")]
+		[SwaggerResponse(HttpStatusCode.BadRequest, typeof(string), Description = "Invalid model")]
+		public async Task<IActionResult> ChangeFirstName([FromBody]ChangeNameModel model)
+		{
+			Task<bool> result = Task.FromResult(
+				string.IsNullOrEmpty(model.NewName));
+
+			return await result ? BadRequest("Is null or empty name") : (IActionResult)NoContent();
+		}
+
+		[HttpPut, Route("changename/middle")]
+		[SwaggerResponse(HttpStatusCode.NoContent, typeof(void), Description = "All OK")]
+		[SwaggerResponse(HttpStatusCode.BadRequest, typeof(string), Description = "Invalid model")]
+		public async Task<IActionResult> ChangeMiddleName([FromBody]ChangeNameModel model)
+		{
+			Task<bool> result = Task.FromResult(
+				string.IsNullOrEmpty(model.NewName));
+
+			return await result ? BadRequest("Is null or empty name") : (IActionResult)NoContent();
+		}
+
+		[HttpPut, Route("changename/middle")]
+		[SwaggerResponse(HttpStatusCode.NoContent, typeof(void), Description = "All OK")]
+		[SwaggerResponse(HttpStatusCode.BadRequest, typeof(string), Description = "Invalid model")]
+		public async Task<IActionResult> ChangeLastName([FromBody]ChangeNameModel model)
+		{
+			Task<bool> result = Task.FromResult(
+				string.IsNullOrEmpty(model.NewName));
+
+			return await result ? BadRequest("Is null or empty name") : (IActionResult)NoContent();
+		}
+	}
+}
---
+++ KvitkouNet/KvitkouNet.Web/Models/UserSettingsModels/ChangeNameModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KvitkouNet.Web.Models
+{
+	/// <summary>
+	/// Модель для изменения имени
+	/// </summary>
+	public class ChangeNameModel
+	{
+		/// <summary>
+		/// Новое имя
+		/// </summary>
+		public string NewName { get; set; }
+	}
+}
