092de658a2cf5496f446c65aac1bbdd49f48b483
Roma Kolyago <romakolyago18@gmail.com>
2018-12-19 22:35:53 +0300

    + add TicketController and AddTicket method

---
+++ KvitkouNet/KvitkouNet.Web/Controllers/TicketController.cs
@@ -0,0 +1,27 @@
+﻿using KvitkouNet.Logic.Common.Models.Ticket;
+using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace KvitkouNet.Web.Controllers
+{
+    [Route("api/tickets")]
+    public class TicketController : Controller
+    {
+        [HttpPost]
+        [Route("add")]
+        [SwaggerResponse(HttpStatusCode.Created, typeof(object), Description = "Ticket created")]
+        [SwaggerResponse(HttpStatusCode.Forbidden, typeof(void), Description = "Access error")]
+        [SwaggerResponse(HttpStatusCode.BadRequest, typeof(string), Description = "Invalid model")]
+        public async Task<IActionResult> AddTicket([FromBody] TicketModel ticketModel)
+        {
+            var result = Task.FromResult(ModelState.IsValid);
+            return await result
+                ? (IActionResult)Created(ticketModel.TicketId.ToString(), ticketModel)
+                : BadRequest("Model not valid");
+        }
+    }
+}
