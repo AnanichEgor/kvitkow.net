408f274dad02ce34059f06c5538f304a829f67b6
romaklayt <romakolyago18@gmail.com>
2018-12-24 18:22:54 +0300

    * register TicketService

--- KvitkouNet/KvitkouNet.Web/Startup.cs
+++ KvitkouNet/KvitkouNet.Web/Startup.cs
@@ -32,6 +32,7 @@ namespace KvitkouNet.Web
             services.AddSwaggerDocument();
             services.RegisterUserServices();
             services.RegisterSecurityService();
+            services.RegisterTicketService();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
