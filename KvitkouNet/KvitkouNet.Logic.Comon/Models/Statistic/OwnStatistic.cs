44df49cf910095f8dc1c044301aec81794113bdf
Ignat Anikovich <tanokina86@gmail.com>
2018-12-24 15:21:03 +0300

    add some classes to Statistic Models

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Statistic/OwnStatistic.cs
@@ -0,0 +1,30 @@
+﻿using KvitkouNet.Logic.Common.Models.UserManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvitkouNet.Logic.Common.Models.Statistic
+{
+    public class OwnStatistic
+    {
+        /// <summary>
+        /// Provides total completed offers count
+        /// </summary>
+        public IEnumerable<Offer> TotalOffers { get; set; }
+
+        /// <summary>
+        /// Provides total sell tickets count
+        /// </summary>
+        public IEnumerable<Ticket.Ticket> RealisedTickets { get; set; }
+
+        /// <summary>
+        /// Provides total tickets, which have been presented
+        /// </summary>
+        public IEnumerable<Ticket.Ticket> DonatedTickets { get; set; }
+
+        /// <summary>
+        /// Provides users, who get in the black list
+        /// </summary>
+        public IDictionary<User, string> BannedUsers { get; set; }
+    }
+}
