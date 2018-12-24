b92c8e6b951bbdb173d2e39fb17485e372a621bf
Marina <for-marina@mail.ru>
2018-12-24 16:37:23 +0300

    + add IUserSettingsService

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Services/UserSettings/IUserSettingsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvitkouNet.Logic.Common.Services.UserSettings
+{
+	/// <summary>
+	/// Сервич для работы с настройками пользователя
+	/// </summary>
+	public interface IUserSettingsService: IDisposable
+	{
+
+	}
+}
