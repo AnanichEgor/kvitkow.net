94f1c0a0068f625d04cc739a66e7f4ef4ebb7897
Ignat Anikovich <tanokina86@gmail.com>
2018-12-25 17:38:47 +0300

    add some Models to payment feature

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Models/Payment Management/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using KvitkouNet.Logic.Common.Enums;
+using KvitkouNet.Logic.Common.Models;
+
+namespace KvitkouNet.Logic.Common.Models.PaymentManagement
+{
+    /// <summary>
+    /// Provides transaction`s data
+    /// </summary>
+    public class Transaction
+    {
+        /// <summary>
+        /// Transaction unique number
+        /// </summary>
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// Currency type
+        /// </summary>
+        public CurrencyType Currency { get; set; }
+
+        ///<summary>
+        ///Ammount 
+        ///</summary>
+        public double Ammount { get; set; }
+
+        ///<summary>
+        ///Description
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
