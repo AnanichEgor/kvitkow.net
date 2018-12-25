e3602e6dcee1c84bab00f42e75aba39b17daacc1
Bakharev Mikhail <baharev96@list.ru>
2018-12-25 17:26:17 +0300

    + added logService interface with 1 method

---
+++ KvitkouNet/KvitkouNet.Logic.Comon/Services/Logging/ILoggingService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using KvitkouNet.Logic.Common.Dtos.Logging;
+using KvitkouNet.Logic.Common.Models.Logging;
+
+namespace KvitkouNet.Logic.Common.Services.Logging
+{
+	public interface ILoggingService
+	{
+		Task<IEnumerable<AccountLogEntry>> Get(AccountLogsFilterDto accountLogsFilter);
+	}
+}
