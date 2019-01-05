11b6f390279b4012a2bdb71e6d63b58b317dd16f
Rodion <dreyevgo@gmail.com>
2019-01-05 12:57:59 +0300

    +add TicketContext

---
+++ KvitkouNet/TicketManagement/TicketManagement.Data/Context/TicketContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketManagement.Logic.Common.Models;
+
+namespace TicketManagement.Data.Context
+{
+    public class TicketContext: DbContext
+    {
+        public TicketContext(DbContextOptions<TicketContext> options)
+       : base(options)
+        {
+
+        }
+        public DbSet<Ticket> Tickets { get; set; }
+      
+    }
+}
