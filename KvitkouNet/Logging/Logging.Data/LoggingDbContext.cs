cf88d703d4fccc7e4629d4dc88bb8238a96f9138
Alexander Pashnikov <dr.pashnikov@google.com>
2019-01-07 02:47:29 +0300

    added loggingdbcontext class

---
+++ KvitkouNet/Logging/Logging.Data/LoggingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Logging.Data
+{
+    public sealed class LoggingDbContext : DbContext
+    {
+        public LoggingDbContext(DbContextOptions<LoggingDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
