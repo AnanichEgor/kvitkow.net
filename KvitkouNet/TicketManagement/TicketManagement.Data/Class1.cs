035c1d8308383110bbc0e6fbab4acd51da8a367e
romaklayt <romakolyago18@gmail.com>
2019-01-07 13:29:29 +0300

    + add models in Data

--- KvitkouNet/TicketManagement/TicketManagement.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TicketManagement.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ KvitkouNet/TicketManagement/TicketManagement.Data/Models/Address.cs
@@ -0,0 +1,35 @@
+﻿namespace TicketManagement.Logic.Common.Models
+{
+    public class Address
+    {
+        /// <summary>
+        ///     Id адреса
+        /// </summary>
+        public string AddressId { get; set; }
+        
+        /// <summary>
+        ///     Страна проживания
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        ///     Город
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        ///     Улица
+        /// </summary>
+        public string Street { get; set; }
+
+        /// <summary>
+        ///     Номер дома, корпус
+        /// </summary>
+        public string House { get; set; }
+
+        /// <summary>
+        ///     Номер квартиры
+        /// </summary>
+        public string Flat { get; set; }
+    }
+}
