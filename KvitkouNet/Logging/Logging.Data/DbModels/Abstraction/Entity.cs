b11dd75ed54038b4ddec22d21e1c67ac21d78f21
Alexander Pashnikov <dr.pashnikov@google.com>
2019-01-07 03:23:32 +0300

    added base dbmodel class

---
+++ KvitkouNet/Logging/Logging.Data/DbModels/Abstraction/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Logging.Data.DbModels.Abstraction
+{
+    /// <summary>
+    /// Базовый класс для всех моделей БД
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    public abstract class Entity<TKey> where TKey : struct
+    {
+        public TKey Id { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime? Modified { get; set; }
+    }
+}
