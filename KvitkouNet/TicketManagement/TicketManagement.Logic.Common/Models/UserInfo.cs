afed38b47361154741cf23a39894b1c686eb8e3c
romaklayt <romakolyago18@gmail.com>
2019-01-07 12:30:20 +0300

    * change userinfo model

--- KvitkouNet/TicketManagement/TicketManagement.Logic.Common/Models/UserInfo.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Logic.Common/Models/UserInfo.cs
@@ -1,33 +1,25 @@
 ﻿namespace TicketManagement.Logic.Common.Models
 {
     /// <summary>
-    ///     Contains minimal information about user indexed by Elasticsearch
+    ///     Вспомогательный класс пользователя для тикета
     /// </summary>
     public class UserInfo
     {
         /// <summary>
-        ///     Id инфоюзера
+        ///     Id юзера
         /// </summary>
         public string UserInfoId { get; set; }
 
         /// <summary>
-        ///     Gets or sets the user identifier.
-        /// </summary>
-        public int UserId { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the first name.
+        ///     Имя юзера
         /// </summary>
         public string FirstName { get; set; }
 
         /// <summary>
-        ///     Gets or sets the last name.
+        ///     Фамилия юзера
         /// </summary>
         public string LastName { get; set; }
 
-        /// <summary>
-        ///     Gets or sets the user rating.
-        /// </summary>
-        public double Rating { get; set; }
+       
     }
 }
