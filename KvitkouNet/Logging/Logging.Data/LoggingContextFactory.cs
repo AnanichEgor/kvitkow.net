4691688307d2e0d645875ba898ac0c287b114b29
Alexander Pashnikov <dr.pashnikov@google.com>
2019-01-07 03:04:35 +0300

    added context factory

---
+++ KvitkouNet/Logging/Logging.Data/LoggingContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Logging.Data
+{
+    public class LoggingContextFactory : IDesignTimeDbContextFactory<LoggingDbContext>
+    {
+        public LoggingDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LoggingDbContext>();
+
+            optionsBuilder.UseSqlite("Data Source=Logs.db");
+
+            return new LoggingDbContext(optionsBuilder.Options);
+        }
+    }
+}
