10ee46e69ce760a15bd674ba92753bb961e475db
Bakharev Mikhail <baharev96@list.ru>
2019-01-09 22:58:34 +0300

    + added base seeding

---
+++ KvitkouNet/Logging/Logging.Data/Fakers/InternalErrorLogEntryFaker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Bogus;
+using Logging.Data.DbModels;
+
+namespace Logging.Data.Fakers
+{
+	public static class InternalErrorLogEntryFaker
+	{
+		private static Faker<InternalErrorLogEntryDbModel> _faker;
+
+		static InternalErrorLogEntryFaker()
+		{
+			_faker = new Faker<InternalErrorLogEntryDbModel>();
+			_faker.RuleFor(_ => _.Content, f => f.Lorem.Sentence(wordCount: 5));
+			_faker.RuleFor(_ => _.InnerExceptionString, f => f.Lorem.Sentence(wordCount: 5));
+			_faker.RuleFor(_ => _.Message, f => f.Lorem.Sentence(wordCount: 5));
+			_faker.RuleFor(_ => _.Source, f => f.Lorem.Sentence(wordCount: 5));
+			_faker.RuleFor(_ => _.StackTrace, f => f.Lorem.Sentence(wordCount: 10));
+			_faker.RuleFor(_ => _.TargetSiteName, f => f.Lorem.Word());
+			_faker.RuleFor(_ => _.TypeName, f => f.Lorem.Word());
+			_faker.RuleFor(_ => _.HResult, f => f.Random.Int(-100, 100));
+		}
+
+		public static IEnumerable<InternalErrorLogEntryDbModel> Generate(int count = 10)
+			=> _faker.Generate(count);
+	}
+}
