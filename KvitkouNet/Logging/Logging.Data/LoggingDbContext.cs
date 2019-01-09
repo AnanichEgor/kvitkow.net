1209860bc949dc8677befc1d0cea17e86f745979
Bakharev Mikhail <baharev96@list.ru>
2019-01-09 22:24:11 +0300

    -removed unnucessary EnsureCreating

--- KvitkouNet/Logging/Logging.Data/LoggingDbContext.cs
+++ KvitkouNet/Logging/Logging.Data/LoggingDbContext.cs
@@ -3,23 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Logging.Data
 {
-    public sealed class LoggingDbContext : DbContext
-    {
-        public LoggingDbContext(DbContextOptions<LoggingDbContext> options) : base(options)
-        {
-            Database.EnsureCreated();
-        }
+	public sealed class LoggingDbContext : DbContext
+	{
+		public LoggingDbContext(DbContextOptions<LoggingDbContext> options) : base(options)
+		{
+		}
 
-        public DbSet<AccountLogEntryDbModel> AccountLogEntries { get; set; }
+		public DbSet<AccountLogEntryDbModel> AccountLogEntries { get; set; }
 
-        public DbSet<InternalErrorLogEntryDbModel> InternalErrorLogEntries { get; set; }
+		public DbSet<InternalErrorLogEntryDbModel> InternalErrorLogEntries { get; set; }
 
-        public DbSet<PaymentLogEntryDbModel> PaymentLogEntries { get; set; }
+		public DbSet<PaymentLogEntryDbModel> PaymentLogEntries { get; set; }
 
-        public DbSet<SearchQueryLogEntryDbModel> SearchQueryLogEntries { get; set; }
+		public DbSet<SearchQueryLogEntryDbModel> SearchQueryLogEntries { get; set; }
 
-        public DbSet<TicketActionLogEntryDbModel> TicketActionLogEntries { get; set; }
+		public DbSet<TicketActionLogEntryDbModel> TicketActionLogEntries { get; set; }
 
-        public DbSet<TicketDealLogEntryDbModel> TicketDealLogEntries { get; set; }
-    }
+		public DbSet<TicketDealLogEntryDbModel> TicketDealLogEntries { get; set; }
+	}
 }
