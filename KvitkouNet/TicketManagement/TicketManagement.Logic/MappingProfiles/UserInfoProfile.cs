bf7bc57b9b0891314c9830b2f74466c4aec62624
Rodion <dreyevgo@gmail.com>
2019-01-09 10:45:23 +0300

    +add AutoMapper and MappingProfile dir

---
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/MappingProfiles/UserInfoProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace TicketManagement.Logic.MappingProfiles
+{
+    class UserInfoProfile: Profile
+    {
+    }
+}
--- KvitkouNet/TicketManagement/TicketManagement.Web/Startup.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +32,7 @@ namespace TicketManagement.Web
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSwaggerDocument();
             services.RegisterTicketService();
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
