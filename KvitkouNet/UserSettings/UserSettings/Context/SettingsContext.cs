d0dd4e0413e89f5d288b6efb70d6fb3f2c7ca28c
Marina <for-marina@mail.ru>
2019-01-09 07:44:19 +0300

    + add context

---
+++ KvitkouNet/UserSettings/UserSettings/Context/SettingsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserSettings.Context
+{
+	public class SettingsContext: DbContext
+	{
+		public SettingsContext(DbContextOptions options)
+			: base(options)
+		{
+
+		}
+	}
+}
