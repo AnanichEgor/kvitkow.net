feb1a9b1677f178f00090aa379359000b0537dc3
romaklayt <romakolyago18@gmail.com>
2019-01-10 18:50:18 +0300

    * now all related types are returned

--- KvitkouNet/TicketManagement/TicketManagement.Logic/Validators/TicketValidator.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Validators/TicketValidator.cs
@@ -11,6 +11,18 @@ namespace TicketManagement.Logic.Validators
         public TicketValidator()
         {
             RuleFor(ticket => ticket.Name).NotEmpty().Length(5, 100);
+            RuleFor(ticket => ticket.AdditionalData).NotEmpty();
+            RuleFor(ticket => ticket.SellerPhone).NotEmpty();
+            RuleFor(ticket => ticket.User.FirstName).NotEmpty();
+            RuleFor(ticket => ticket.User.LastName).NotEmpty();
+            RuleFor(ticket => ticket.LocationEvent.City).NotEmpty();
+            RuleFor(ticket => ticket.LocationEvent.Country).NotEmpty();
+            RuleFor(ticket => ticket.LocationEvent.Street).NotEmpty();
+            RuleFor(ticket => ticket.LocationEvent.House).NotEmpty();
+            RuleFor(ticket => ticket.SellerAdress.City).NotEmpty();
+            RuleFor(ticket => ticket.SellerAdress.Country).NotEmpty();
+            RuleFor(ticket => ticket.SellerAdress.Street).NotEmpty();
+            RuleFor(ticket => ticket.SellerAdress.House).NotEmpty();
 
         }
     }
