229e98038d05d61f437862d6d03506a841c0fd04
Alexander Pashnikov <dr.pashnikov@google.com>
2019-01-11 01:20:02 +0300

    implemented IRepository as BaseRepository

--- KvitkouNet/Logging/Logging.Data/Infrastructure/IRepository.cs
+++ KvitkouNet/Logging/Logging.Data/Infrastructure/IRepository.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Logging.Data.DbModels.Abstraction;
+using Microsoft.EntityFrameworkCore;
 
 namespace Logging.Data.Infrastructure
 {
     public interface IRepository<TEntity, in TKey> : IDisposable where TEntity : Entity<TKey>
     {
+        DbSet<TEntity> DbSet { get; }
+
         Task<TEntity> GetByIdAsync(TKey id);
 
         Task<IEnumerable<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] include);
@@ -23,5 +26,7 @@ namespace Logging.Data.Infrastructure
         Task DeleteAsync(TKey id);
 
         Task<int> CountAsync(Expression<Func<TEntity, bool>> filter);
+
+        Task SaveAsync();
     }
 }
