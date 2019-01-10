8b600cbdef89f908908e8d9f0b413b0fbb02ca9d
Alexander Pashnikov <dr.pashnikov@google.com>
2019-01-11 01:11:04 +0300

    added IRepository interface

---
+++ KvitkouNet/Logging/Logging.Data/Infrastructure/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Logging.Data.DbModels.Abstraction;
+
+namespace Logging.Data.Infrastructure
+{
+    public interface IRepository<TEntity, in TKey> : IDisposable where TEntity : Entity<TKey>
+    {
+        Task<TEntity> GetByIdAsync(TKey id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] include);
+
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> filter, params Expression<Func<TEntity, object>>[] include);
+
+        void Create(TEntity instance);
+
+        void Update(TEntity instance);
+
+        void Delete(TEntity instance);
+
+        Task DeleteAsync(TKey id);
+
+        Task<int> CountAsync(Expression<Func<TEntity, bool>> filter);
+    }
+}
