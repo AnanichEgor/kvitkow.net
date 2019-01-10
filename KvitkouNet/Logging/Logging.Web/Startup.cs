846658e036e1eb44a55fddea1c7afd79ca2b6e57
Alexander Pashnikov <dr.pashnikov@google.com>
2019-01-11 00:45:50 +0300

    added mapping profiles to the main mapping configuration with assembly scanning

--- KvitkouNet/Logging/Logging.Web/Startup.cs
+++ KvitkouNet/Logging/Logging.Web/Startup.cs
@@ -1,6 +1,8 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
 using Logging.Logic.Extensions;
-using Logging.Logic.MappingProfiles;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -23,10 +25,13 @@ namespace Logging.Web
 		{
 			services.AddDbContext();
 
-			services.AddAutoMapper(cfg =>
-			{
-				cfg.AddProfile<InternalErrorLogProfile>();
-			});
+		    var assemblyNamesToScan = Assembly
+		        .GetEntryAssembly()
+		        .GetReferencedAssemblies()
+		        .Where(an => an.FullName.StartsWith("Logging", StringComparison.OrdinalIgnoreCase))
+		        .Select(an => an.FullName);
+
+            services.AddAutoMapper(cfg => cfg.AddProfiles(assemblyNamesToScan));
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
