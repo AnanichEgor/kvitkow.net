0ae4d633497efdfc98a98fea5a4945b4d7f92b2e
Egor Ananich <ev.ananich@gmail.com>
2019-01-12 18:11:29 +0300

    + Add context and first migration

---
+++ KvitkouNet/StatisticOnline/StatisticOnline.Data/Context/WebApiContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StatisticOnline.Data.Context
+{
+    public class WebApiContext: DbContext
+    {
+        public WebApiContext(DbContextOptions<WebApiContext> options) : base(options)
+        {
+
+        }
+    }
+}
