e49d99825677ae6953d48b96bc973e2540e51a3d
Egor Ananich <ev.ananich@gmail.com>
2019-01-12 17:39:43 +0300

    + Add ServiceExtentions

---
+++ KvitkouNet/StatisticOnline/StatisticOnline.Logic/Services/ServiceExtentions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatisticOnline.Logic.Services
+{
+    public class ServiceExtentions
+    {
+    }
+}
