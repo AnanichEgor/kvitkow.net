52faf0a1f87e153b4de2a9294382e58ee3ac5d9a
romaklayt <romakolyago18@gmail.com>
2019-01-12 18:33:24 +0300

    * tweaked validator

--- KvitkouNet/TicketManagement/TicketManagement.Logic/Validators/TicketValidator.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Validators/TicketValidator.cs
@@ -11,18 +11,18 @@ namespace TicketManagement.Logic.Validators
         public TicketValidator()
         {
             RuleFor(ticket => ticket.Name).NotEmpty().Length(5, 100);
-            RuleFor(ticket => ticket.AdditionalData).NotEmpty();
-            RuleFor(ticket => ticket.SellerPhone).NotEmpty();
-            RuleFor(ticket => ticket.User.FirstName).NotEmpty();
-            RuleFor(ticket => ticket.User.LastName).NotEmpty();
-            RuleFor(ticket => ticket.LocationEvent.City).NotEmpty();
-            RuleFor(ticket => ticket.LocationEvent.Country).NotEmpty();
-            RuleFor(ticket => ticket.LocationEvent.Street).NotEmpty();
-            RuleFor(ticket => ticket.LocationEvent.House).NotEmpty();
-            RuleFor(ticket => ticket.SellerAdress.City).NotEmpty();
-            RuleFor(ticket => ticket.SellerAdress.Country).NotEmpty();
-            RuleFor(ticket => ticket.SellerAdress.Street).NotEmpty();
-            RuleFor(ticket => ticket.SellerAdress.House).NotEmpty();
+            RuleFor(ticket => ticket.AdditionalData).MaximumLength(240);
+            RuleFor(ticket => ticket.SellerPhone).NotEmpty().Length(6,9);
+            RuleFor(ticket => ticket.User.FirstName).NotEmpty().Length(3,12);
+            RuleFor(ticket => ticket.User.LastName).NotEmpty().Length(3,15);
+            RuleFor(ticket => ticket.LocationEvent.City).NotEmpty().Length(3,15);
+            RuleFor(ticket => ticket.LocationEvent.Country).NotEmpty().Length(3,15);
+            RuleFor(ticket => ticket.LocationEvent.Street).NotEmpty().Length(3,15);
+            RuleFor(ticket => ticket.LocationEvent.House).NotEmpty().Length(1,6);
+            RuleFor(ticket => ticket.SellerAdress.City).NotEmpty().Length(3, 15);
+            RuleFor(ticket => ticket.SellerAdress.Country).NotEmpty().Length(3, 15);
+            RuleFor(ticket => ticket.SellerAdress.Street).NotEmpty().Length(3, 15);
+            RuleFor(ticket => ticket.SellerAdress.House).NotEmpty().Length(1, 6);
 
         }
     }
