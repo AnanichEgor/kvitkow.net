d21f99782d6c719eced4b153592924fe222cc027
Egor Ananich <ev.ananich@gmail.com>
2019-01-12 17:55:54 +0300

    + Add StatisticOnlineModel

---
+++ KvitkouNet/StatisticOnline/StatisticOnline.Data/Models/StatisticOnlineModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatisticOnline.Data.Models
+{
+    public class StatisticOnlineModel
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// число всех пользователей на сайте Online
+        /// </summary>
+        public int CountAll { get; set; }
+
+        /// <summary>
+        /// число зарегистрированных пользователей
+        /// </summary>
+        public int CountRegistered { get; set; }
+
+        /// <summary>
+        /// число гостей на сайте Online
+        /// </summary>
+        public int CountGuest { get; set; }
+
+        /// <summary>
+        /// Тата и время на которую сформированы записи
+        /// </summary>
+        public DateTime CurrenTime { get; set; }
+    }
+}
