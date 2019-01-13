c3024afb53f74770abb88fc1011dffde8673cc0f
Bakharev Mikhail <baharev96@list.ru>
2019-01-13 21:57:02 +0300

    + added simple controller for working with user management

---
+++ KvitkouNet/AdminPanel/AdminPanel.Web/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Web.Controllers
+{
+	/// <summary>
+	/// Контроллер для работы с пользователями через панель администратора
+	/// </summary>
+	[Route("api/admin/users")]
+	public class UserController : Controller
+	{
+		public UserController()
+		{
+			
+		}
+
+		/// <summary>
+		/// Получает пользователей для просмотра с помощью панели администратора
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		public async Task<IActionResult> GetUsers()
+		{
+			return Ok();
+		}
+
+		[HttpPut]
+		[Route("ban/{id}")]
+		public async Task<IActionResult> BanUser()
+		{
+			return NoContent();
+		}
+
+		[HttpPut]
+		[Route("unban/{id}")]
+		public async Task<IActionResult> UnbanUser()
+		{
+			return NoContent();
+		}
+	}
+}
