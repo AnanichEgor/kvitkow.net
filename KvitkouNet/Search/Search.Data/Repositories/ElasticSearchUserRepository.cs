91eada9636b8a3f00407564c4c16d24732df2957
Valeria <valiretti@gmail.com>
2019-01-14 00:04:45 +0300

    + added repositories for ElasticSearch

---
+++ KvitkouNet/Search/Search.Data/Repositories/ElasticSearchUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+using Search.Logic.Common.Models;
+
+namespace Search.Data.Repositories
+{
+    public class ElasticSearchUserRepository : ElasticSearchRepository<UserInfo>, IUserRepository
+    {
+        public ElasticSearchUserRepository(IElasticClient client) : base(client)
+        {
+        }
+
+        Task<SearchResult<UserInfo>> IUserRepository.SearchAsync(UserSearchRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
