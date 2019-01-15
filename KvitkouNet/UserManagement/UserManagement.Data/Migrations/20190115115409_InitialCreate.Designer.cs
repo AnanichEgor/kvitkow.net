dc4f5d10b9908d0e8b5a4587ab4b75325cc6e8dc
Alex160881 <Alex160881@gmail.com>
2019-01-15 15:51:51 +0300

    * fix Migration, del reference data from web

--- KvitkouNet/UserManagement/UserManagement.Data/Migrations/20190115115409_InitialCreate.Designer.cs
+++ KvitkouNet/UserManagement/UserManagement.Data/Migrations/20190115115409_InitialCreate.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserManagement.Data.Context;
 
 namespace UserManagement.Data.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20190115115409_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- KvitkouNet/UserManagement/UserManagement.Logic.Common/ServiceExtentions.cs
+++ KvitkouNet/UserManagement/UserManagement.Logic.Common/ServiceExtentions.cs
@@ -1,6 +1,15 @@
 ﻿using UserManagement.Logic.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using FluentValidation;
+using UserManagement.Logic.Validators;
+using UserManagement.Logic.Models;
+using AutoMapper;
+using UserManagement.Data.Fakers;
+using UserManagement.Data.Context;
+using UserManagement.Logic.MappingProfiles;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace UserManagement.Logic
 {
@@ -8,6 +17,23 @@ namespace UserManagement.Logic
     {
         public static IServiceCollection RegisterUserServices(this IServiceCollection services)
         {
+            var o = new DbContextOptionsBuilder<UserContext>();
+            o.UseSqlite("Data Source=./UserDatabase.db");
+
+            using (var context = new UserContext(o.Options))
+            {
+                context.Database.Migrate();
+                if (!context.Users.Any())
+                {
+                    context.Users.AddRange(UserFaker.Generate());
+                    context.SaveChanges();
+                }
+            }
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.AddProfile<UserProfile>();
+            });
+            services.AddScoped<IValidator<User>, UserValidator>();
             var mock = new Mock<IUserService>();
 
             services.AddScoped<IUserService>(_ => mock.Object);
