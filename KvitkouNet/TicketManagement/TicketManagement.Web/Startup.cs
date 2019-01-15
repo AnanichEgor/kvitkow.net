d23ce337350a01d1dba255922e7d91cb2c6b587f
Rodion <dreyevgo@gmail.com>
2019-01-15 16:14:49 +0300

    *fix errrors

--- KvitkouNet/TicketManagement/TicketManagement.Web/Startup.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Web/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using AutoMapper;
 using FluentValidation;
 using Microsoft.AspNetCore.Builder;
@@ -10,13 +7,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using TicketManagement.Data.Context;
+using TicketManagement.Data.Fakes;
+using TicketManagement.Data.Repositories;
 using TicketManagement.Logic;
 using TicketManagement.Logic.MappingProfiles;
 using TicketManagement.Logic.Models;
+using TicketManagement.Logic.Models.Enums;
+using TicketManagement.Logic.Services;
 using TicketManagement.Logic.Validators;
+
 namespace TicketManagement.Web
 {
     public class Startup
@@ -30,42 +30,16 @@ namespace TicketManagement.Web
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<TicketContext>(opt => opt.UseSqlite("Data Source=./TicketDatabase.db"));
-
-            var o = new DbContextOptionsBuilder<TicketContext>();
-            o.UseSqlite("Data Source=./MyFirstDatabase.db");
-
-            using (var ctx = new TicketContext(o.Options))
-            {
-                ctx.Database.Migrate();
-
-                if (!ctx.Tickets.Any())
-                {                    
-                    ctx.SaveChanges();
-                }
-            }
-
+        {           
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddSwaggerDocument();
-            services.RegisterTicketService();
-            services.AddAutoMapper(cfg =>
-            {
-                cfg.AddProfile<TicketProfile>();
-                cfg.AddProfile<AddressProfile>();
-            });
-
-            services.AddScoped<IValidator<Ticket>, TicketValidator>();
+            services.AddSwaggerDocument();             
+            services.RegisterTicketService();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
             app.UseSwagger().UseSwaggerUi3();
             app.UseMvc();
         }
