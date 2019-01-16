c223eba5a74642ded076ce38c361c6a87f9d746b
Bakharev Mikhail <baharev96@list.ru>
2019-01-16 22:13:49 +0300

    + added logging controller

---
+++ KvitkouNet/AdminPanel/AdminPanel.Web/Controllers/LoggingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Web.Controllers
+{
+	/// <summary>
+	/// Контроллер для работы с логами через панель администратора
+	/// </summary>
+	[Route("api/admin/logs")]
+	public class LoggingController : Controller
+	{
+		/// <summary>
+		/// Возвращает список залогированных ошибок
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet("errors")]
+		public IActionResult Index()
+		{
+			return Ok();
+		}
+	}
+}
