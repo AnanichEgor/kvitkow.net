05998ebcc19a7aa4427d4c772c2070f7087a40af
romaklayt <romakolyago18@gmail.com>
2019-01-17 17:34:32 +0300

    * changed TicketContext

--- KvitkouNet/TicketManagement/TicketManagement.Data/Context/TicketContext.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Data/Context/TicketContext.cs
@@ -9,21 +9,29 @@ namespace TicketManagement.Data.Context
         {
         }
         public DbSet<TicketDb> Tickets { get; set; }
-        public DbSet<AddressDb> Adresses { get; set; }
+        public DbSet<SellerAddress> SellerAddresses { get; set; }
+
+        public DbSet<LocationAddress> LocationAddresses { get; set; }
         public DbSet<UserInfoDb> UserInfos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<LocationAddress>()
+                .ToTable("LocationAddresses")
+                .HasMany(p => p.Tickets)
+                .WithOne(p => p.LocationEvent)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<SellerAddress>()
+                .ToTable("SellerAddresses")
+                .HasMany(p => p.Tickets)
+                .WithOne(p => p.SellerAdress)
+                .OnDelete(DeleteBehavior.Restrict);
             modelBuilder.Entity<TicketDb>()
                 .HasOne(p => p.User)
                 .WithMany(b => b.UserTickets)
-                .OnDelete(DeleteBehavior.SetNull);
-
-            modelBuilder.Entity<TicketDb>()
-               .HasOne(p => p.SellerAdress)
-               .WithMany(b => b.Tickets)
-               .OnDelete(DeleteBehavior.SetNull);
+                .OnDelete(DeleteBehavior.Restrict);
         }
+       
     }
 
 }
