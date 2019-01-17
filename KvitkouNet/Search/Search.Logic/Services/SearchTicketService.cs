22b76ac6b8ce518d6625e328200a05765c8136f8
Valeria <valiretti@gmail.com>
2019-01-17 22:04:48 +0300

    + added Search services

---
+++ KvitkouNet/Search/Search.Logic/Services/SearchTicketService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Search.Data.Repositories;
+using Search.Logic.Common.Models;
+
+namespace Search.Logic.Services
+{
+    public class SearchTicketService : ISearchTicketService
+    {
+        private readonly ITicketRepository _ticketRepository;
+
+        public SearchTicketService(ITicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+
+        public Task<SearchResult<TicketInfo>> Search(TicketSearchRequest request)
+        {
+            return _ticketRepository.SearchAsync(request);
+        }
+
+        public void Dispose()
+        {
+        }
+
+    }
+}
---
+++ KvitkouNet/Search/Search.Logic/Services/SearchUserService.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Search.Data.Repositories;
+using Search.Logic.Common.Models;
+
+namespace Search.Logic.Services
+{
+    public class SearchUserService : ISearchUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public SearchUserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task<SearchResult<UserInfo>> Search(UserSearchRequest request)
+        {
+            return _userRepository.SearchAsync(request);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
