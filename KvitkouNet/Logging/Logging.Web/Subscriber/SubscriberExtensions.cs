f609a2e3490199e39a6f99391c0548caed661e65
Bakharev Mikhail <baharev96@list.ru>
2019-01-20 00:10:13 +0300

    + added clasees for rabbitmq subscription

---
+++ KvitkouNet/Logging/Logging.Web/Subscriber/SubscriberExtensions.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using EasyNetQ;
+using EasyNetQ.AutoSubscribe;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logging.Web.Subscriber
+{
+	public static class SubscriberExtensions
+	{
+		public static IApplicationBuilder UseSubscriber(this IApplicationBuilder app, string prefix,
+			params Assembly[] assembly)
+		{
+			var services = app.ApplicationServices.CreateScope().ServiceProvider;
+
+			var lifetime = services.GetService<IApplicationLifetime>();
+			var bus = services.GetService<IBus>();
+
+			lifetime.ApplicationStarted.Register(() =>
+			{
+				var subscriber = new AutoSubscriber(bus, prefix);
+				subscriber.Subscribe(assembly);
+				subscriber.SubscribeAsync(assembly);
+			});
+
+			lifetime.ApplicationStopped.Register(() => bus.Dispose());
+
+			return app;
+		}
+	}
+}
