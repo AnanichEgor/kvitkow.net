bdd5f6b1a88600d04b5824829e4c20af01b83a5f
Yauheny <Syabra.Bel@gmail.com>
2019-01-20 00:31:05 -0900

    + empty project
    + empty class
    + empty interface

---
+++ KvitkouNet/Dashboard/DashboardMicroservice/Dashboard.Data/DbModels/UserInfoDb.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dashboard.Data.DbModels
+{
+    class UserInfoDb
+    {
+    }
+}
---
+++ KvitkouNet/Dashboard/DashboardMicroservice/Dashboard.Logic/Models/News.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Dashboard.Logic
+{
+    public class Class1
+    {
+    }
+}
