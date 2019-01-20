b8cd44d01cc6fa69fd9083dc3f5699a6b5e9c773
Andrei <agobrik92@gmail.com>
2019-01-20 15:59:53 +0300

    refactoring class  ChatRepository

--- KvitkouNet/Chat/Chat.Data/Repositories/ChatRepository.cs
+++ KvitkouNet/Chat/Chat.Data/Repositories/ChatRepository.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Chat.Data.Context;
 using Chat.Data.DbModels;
 using Microsoft.EntityFrameworkCore;
-using SQLitePCL;
 
 namespace Chat.Data.Repositories
 {
@@ -19,22 +16,21 @@ namespace Chat.Data.Repositories
             _context = context;
         }
 
-        public  Task<SettingsDb> GetUserSettings(string userId)
+        public async Task<SettingsDb> GetUserSettings(string userId)
         {
-            return  _context.Settings
+            return await _context.Settings
                     .SingleOrDefaultAsync( x => x.User.Id.Equals(userId));
         }
 
-        // todo Что возвращать при Update и когда для Save использовать await. И нужно ли в этом случае тогда передавать еще userId?
         public async Task UpdateUserSettings(string userId, SettingsDb settings)
         {
-            _context.Settings.Update(settings);
+            await Task.Run(() =>_context.Settings.Update(settings));
             await _context.SaveChangesAsync();
         }
 
         public async Task UpdateUserRole(string userId, UserDb user)
         {
-            _context.Users.Update(user);
+            await Task.Run(() => _context.Users.Update(user));
             await _context.SaveChangesAsync();
         }
 
@@ -47,56 +43,52 @@ namespace Chat.Data.Repositories
         // todo нужно добавить еще условие для выборки всех приватных комнат и соответственно доработать модель
         public async Task<IEnumerable<RoomDb>> GetRooms(string userId)
         {
-            return _context.Rooms.Where(x => x.IsPrivat == false);
+            return await _context.Rooms.Where(x => x.IsPrivat == false).ToArrayAsync();
         }
 
-        // todo надо разобраться с  асинхронным запуском
         public async Task<IEnumerable<RoomDb>> SearchRoom(string template)
         {
-            return _context.Rooms.Where(x => EF.Functions.Like(x.Name, $"%{template}%"));
+            return await _context.Rooms.Where(x => EF.Functions.Like(x.Name, $"%{template}%")).ToArrayAsync();
         }
 
         public async Task<IEnumerable<MessageDb>> GetMessages(string roomId)
         {
-            return _context.Messages.Where(x => x.RoomId.Equals(roomId));
+            return await _context.Messages.Where(x => x.RoomId.Equals(roomId)).ToArrayAsync();
         }
 
         //todo  здесь where.where - это как условие AND ?
         public async Task<IEnumerable<MessageDb>> SearchMessage(string roomId, string template)
         {
-            return _context.Messages.Where(x => x.Room.Id.Equals(roomId)).Where(x => EF.Functions.Like(x.Text, $"%{template}%"));
+            return await _context.Messages.Where(x => x.Room.Id.Equals(roomId))
+                .Where(x => EF.Functions.Like(x.Text, $"%{template}%")).ToArrayAsync();
         }
 
         public async Task AddMessage(MessageDb message, string roomId)
         {
             message.RoomId = roomId;
             await _context.Messages.AddAsync(message);
+            await _context.SaveChangesAsync();
         }
-        // todo тот же вопрос с update
+
         public async Task UpdateMessage(MessageDb message, string roomId)
         {
-            _context.Messages.Update(message);
+            await  Task.Run(() => _context.Messages.Update(message));
             await _context.SaveChangesAsync();
         }
 
-        // todo как тут удалять правильно?
         public async Task DeleteMessage(string roomId, string messageId)
         {
-            _context.Messages.Remove(new MessageDb() {Id = messageId});
+            await Task.Run(() =>_context.Messages.Remove(new MessageDb() {Id = messageId}));
             await _context.SaveChangesAsync();
         }
 
-        //todo как обновить конкретное поле если для update нужна вся модель ?
         public async Task UpdateSettingIsReeadForMessage(string roomId, string messageId)
         {
-            var oldMessage = _context.Messages.SingleOrDefault(x => x.Id.Equals(messageId));
-            if (oldMessage != null)
+            var message = _context.Messages.SingleOrDefaultAsync(x => x.Id.Equals(messageId));
+            if (message != null)
             {
-                oldMessage.IsRead = true;
-                var newMessage = oldMessage;
-                _context.Messages.Update(newMessage);
+                message.Result.IsRead = true;
             }
-
             await _context.SaveChangesAsync();
         }
     }
