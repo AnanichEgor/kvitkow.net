9432d4d443f62fe647b5a3c923274387f921bf94
Egor Ananich <ev.ananich@gmail.com>
2019-01-20 14:21:05 +0300

    + add MessagesDB

---
+++ KvitkouNet/StatisticUser/StatisticUser.Data/DbModels/MessagesOnSiteDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticUser.Data.DbModels
+{
+    /// <summary>
+    /// Хранит статистику по количеству сообщений
+    /// зарегистрированных пользователей
+    /// </summary>
+    public class MessagesUsersOnSiteDB
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MessageCount { get; set; }
+    }
+}
