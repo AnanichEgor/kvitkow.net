ecb980f707c22510688058c6ae023cd867d2a236
Bakharev Mikhail <baharev96@list.ru>
2019-01-22 23:45:47 +0300

    - removed counter component

--- KvitkouNet/KvitkouNet.Web/ClientApp/src/app/app.module.ts
+++ KvitkouNet/KvitkouNet.Web/ClientApp/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
@@ -17,7 +16,6 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
     FetchDataComponent,
     MenuComponent,
     FooterComponent
@@ -28,7 +26,6 @@ import { FooterComponent } from './footer/footer.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
