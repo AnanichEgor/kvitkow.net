6a8bc8f1afec9c7b90ce647ed8b7d89483a5f14a
Alexander Pashnikov <dr.pashnikov@google.com>
2019-01-23 22:50:50 +0300

    added common messages assembly, added a few message models

---
+++ KvitkouNet.Messages/UserManagement/UserCreationMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvitkouNet.Messages.UserManagement
+{
+    /// <summary>
+    /// Модель сообщения о создании нового пользователя
+    /// </summary>
+    public class UserCreationMessage
+    {
+        /// <summary>
+        /// Имя нового пользователя
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Email пользователя
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Дата и время создания нового пользователя
+        /// </summary>
+        public DateTime Created { get; set; }
+    }
+}
