8e0540e90b634bf5c7478a67a7daddcb47126d78
romaklayt <romakolyago18@gmail.com>
2019-01-24 14:34:35 +0300

    + add mapping profile for Page<TicketDb>, Page<TicketLite>

---
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/MappingProfiles/PageTicketsProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using TicketManagement.Data.DbModels;
+using TicketManagement.Logic.Models;
+
+namespace TicketManagement.Logic.MappingProfiles
+{
+   public class PageTicketsProfile:Profile
+    {
+        public PageTicketsProfile()
+        {
+            CreateMap<Data.DbModels.Page<TicketDb>, Models.Page<TicketLite>>().ReverseMap();
+        }
+    }
+}
