0e4092a4f6e22b4acab6774a42ad40086793cd1a
Alex160881 <Alex160881@gmail.com>
2019-01-24 03:37:49 +0300

    * fix validation

---
+++ KvitkouNet/UserManagement/UserManagement.Logic.Common/Validators/UserRegisterValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using UserManagement.Logic.Models;
+
+namespace UserManagement.Logic.Validators
+{
+    public class UserRegisterValidator : AbstractValidator<UserRegisterModel>
+    {
+        public UserRegisterValidator()
+        {
+            RuleFor(x => x.Username).NotEmpty().Matches(@" ^[a - zA - Z][a - zA - Z0 - 9 - _\.]{5,15}$");
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Password).NotEmpty().Length(6, 12).Matches(@" ^[a - zA - Z][a - zA - Z0 - 9 - _\.]$");
+            RuleFor(x => x.ConfirmPassword).Equal(x => x.Password);
+        }
+    }
+}
