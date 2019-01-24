52fdff2e190a5155f19c827ea779cfaab5c72a93
romaklayt <romakolyago18@gmail.com>
2019-01-24 14:18:44 +0300

    +add page model in logic

---
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Models/Page.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketManagement.Logic.Models
+{
+   public class Page<T>
+    {
+        /// <summary>
+        ///     Текущая страница (приходит с фронта)
+        /// </summary>
+        public int CurrentPage { get; set; }
+
+        /// <summary>
+        ///     Количество записей на странице (приходит с фронта)
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        ///     Количество страниц (вычисляется в репозитории)
+        /// </summary>
+        public int TotalPages { get; set; }
+
+        /// <summary>
+        ///     Список тикетов
+        /// </summary>
+        public List<T> Tickets { get; set; }
+
+        public Page()
+        {
+            Tickets = new List<T>();
+        }
+
+        public Page(IEnumerable<T> records)
+        {
+            Tickets = new List<T>(records);
+        }
+    }
+}
