b8547a68efe3d3deb3989cc29cd4f669ddec4858
Valeria <valiretti@gmail.com>
2019-01-24 21:53:04 +0300

    added search profile for request to save last search

---
+++ KvitkouNet/Search/Search.Logic/MappingProfiles/SearchProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Search.Data.Models;
+using Search.Logic.Common.Models;
+
+namespace Search.Logic.MappingProfiles
+{
+    public class SearchProfile : Profile
+    {
+        public SearchProfile()
+        {
+            CreateMap<SearchRequest, SearchEntity>()
+                .ForMember(entity => entity.SearchTime,
+                    opt => opt.MapFrom(request => DateTime.UtcNow));
+            // todo   .ForMember(entity => entity.UserId);
+        }
+    }
+}
