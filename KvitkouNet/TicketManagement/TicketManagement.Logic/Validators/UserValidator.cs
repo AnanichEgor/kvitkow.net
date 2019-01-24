a6836ccfd5e52429659857a53486d5d764dcfcfc
romaklayt <romakolyago18@gmail.com>
2019-01-24 19:06:30 +0300

    * validators are broken down by file

---
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Validators/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TicketManagement.Logic.Models;
+
+namespace TicketManagement.Logic.Validators
+{
+    /// <summary>
+    ///     Настройка валидатора для юзера
+    /// </summary>
+    public class UserValidator : AbstractValidator<UserInfo>
+    {
+        public UserValidator()
+        {
+            RuleFor(ticket => ticket.FirstName).NotEmpty().Length(3, 12);
+            RuleFor(ticket => ticket.LastName).NotEmpty().Length(3, 15);
+        }
+    }
+}
