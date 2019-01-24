c5a47741d898b55f75ed0f077c5709d84c091a85
romaklayt <romakolyago18@gmail.com>
2019-01-24 14:21:46 +0300

    + add model ticket lite in logic

---
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Models/TicketLite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TicketManagement.Logic.Models
+{
+    public class TicketLite
+    {
+        /// <summary>
+        ///     Пользователь разместивший билет
+        /// </summary>
+        public UserInfo User { get; set; }
+
+        /// <summary>
+        ///     Платный/бесплатный билет
+        /// </summary>
+        public bool Free { get; set; }
+
+        /// <summary>
+        ///     Название билета
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Цена билета
+        /// </summary>
+        public decimal? Price { get; set; }
+
+        /// <summary>
+        ///     Статус билета
+        /// </summary>
+        public TicketStatus Status { get; set; }
+
+        /// <summary>
+        ///     Дата создания билета
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+    }
+}
