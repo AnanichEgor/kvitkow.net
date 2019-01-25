7cfe8cb9a8beaacb53fb6cbc9b66a44c3a5dd752
Rodion Andreev <dreyevgo@gmail.com>
2019-01-25 13:08:10 +0300

    * move components to its own Folder

--- KvitkouNet/KvitkouNet.Web/ClientApp/src/app/app.module.ts
+++ KvitkouNet/KvitkouNet.Web/ClientApp/src/app/app.module.ts
@@ -5,13 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
-import { AdminComponent } from './admin/admin.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { TicketComponent } from './ticket/ticket.component';
-import { TicketFormComponent } from './ticket-form/ticket-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { TicketFormComponent } from './components/ticket-form/ticket-form.component';
 
 @NgModule({
   declarations: [
