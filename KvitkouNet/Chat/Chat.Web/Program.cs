8c3d6a71df1ce5f54a4d99039f5ce8bc0cd22d32
Andrei <agobrik92@gmail.com>
2019-01-26 13:36:35 +0300

    Delete DBContext in Program and add class Helper in Data

--- KvitkouNet/Chat/Chat.Web/Program.cs
+++ KvitkouNet/Chat/Chat.Web/Program.cs
@@ -1,9 +1,5 @@
-﻿using System.Linq;
-using Chat.Data.Context;
-using Chat.Logic.Fakers;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 
 namespace Chat.Web
 {
@@ -11,20 +7,6 @@ namespace Chat.Web
     {
         public static void Main(string[] args)
         {
-            var o = new DbContextOptionsBuilder<ChatContext>();
-            o.UseSqlite("Data Source=f:\\Git\\kvitkou-net\\KvitkouNet\\Chat\\Chat.Web\\ChatDatabase.db");
-
-            using (var ctx = new ChatContext(o.Options))
-            {
-                ctx.Database.Migrate();
-                if (!ctx.Rooms.Any())
-                {
-                    ctx.Rooms.AddRange(RoomFaker.Generate(50));
-                //    ctx.Users.AddRange(UserFaker.Generate(50));
-                    ctx.SaveChanges();
-                }
-            }
-
             CreateWebHostBuilder(args).Build().Run();
         }
 
