7e5f97bc41390c8004046915d8b7150e0b108c5c
Valeria <valiretti@gmail.com>
2019-01-26 17:50:56 +0300

    + added ticket, user consumers; + added messageDispatcher, subscriberExtension

---
+++ KvitkouNet/Search/Search.Web/Subscriber/SubscriberExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using EasyNetQ;
+using EasyNetQ.AutoSubscribe;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Search.Web.Subscriber
+{
+    public static class SubscriberExtensions
+    {
+        public static IApplicationBuilder UseSubscriber(this IApplicationBuilder app, string prefix,
+            params Assembly[] assembly)
+        {
+            var services = app.ApplicationServices.CreateScope().ServiceProvider;
+
+            var lifetime = services.GetService<IApplicationLifetime>();
+            var bus = services.GetService<IBus>();
+
+            lifetime.ApplicationStarted.Register(() =>
+            {
+                var subscriber = new AutoSubscriber(bus, prefix)
+                {
+                    AutoSubscriberMessageDispatcher = new MessageDispatcher(app.ApplicationServices)
+                };
+                subscriber.Subscribe(assembly);
+                subscriber.SubscribeAsync(assembly);
+            });
+
+            lifetime.ApplicationStopped.Register(() => bus.Dispose());
+
+            return app;
+        }
+    }
+}
