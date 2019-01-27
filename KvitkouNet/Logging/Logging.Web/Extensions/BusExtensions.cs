c2efc842f284160cde4dbcce64652704068a0404
Bakharev Mikhail <baharev96@list.ru>
2019-01-27 17:36:45 +0300

    + added new extensions method for subscription

---
+++ KvitkouNet/Logging/Logging.Web/Extensions/BusExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EasyNetQ;
+using EasyNetQ.AutoSubscribe;
+using KvitkouNet.Messages.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logging.Web.Extensions
+{
+	public static class BusExtensions
+	{
+		public static IBus SubscribeAllConsumers(this IBus bus, IServiceProvider services)
+		{
+			var prefix = "ErrorLogging.Added";
+			var internalErrorLogConsumer = services.GetService<IConsumeAsync<InternalErrorLogEntryMessage>>();
+			bus.SubscribeAsync<InternalErrorLogEntryMessage>(prefix, msg => internalErrorLogConsumer.ConsumeAsync(msg));
+			return bus;
+		}
+	}
+}
