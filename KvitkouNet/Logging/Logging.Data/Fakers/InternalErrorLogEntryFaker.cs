9be4d3c36f38f7dfb4b6ad8628e1c078d394d019
Alexander Pashnikov <dr.pashnikov@google.com>
2019-01-29 00:06:29 +0300

    fixed error logs faker

--- KvitkouNet/Logging/Logging.Data/Fakers/InternalErrorLogEntryFaker.cs
+++ KvitkouNet/Logging/Logging.Data/Fakers/InternalErrorLogEntryFaker.cs
@@ -12,6 +12,8 @@ namespace Logging.Data.Fakers
 		{
 			_faker = new Faker<InternalErrorLogEntryDbModel>();
 			_faker.RuleFor(_ => _.Id, f => f.IndexFaker.ToString());
+			_faker.RuleFor(_ => _.ServiceName, f => f.Company.ToString());
+			_faker.RuleFor(_ => _.ExceptionType, f => f.Lorem.Word().ToString());
 			_faker.RuleFor(_ => _.InnerExceptionString, f => f.Lorem.Sentence(wordCount: 5));
 			_faker.RuleFor(_ => _.Message, f => f.Lorem.Sentence(wordCount: 5));
 			_faker.RuleFor(_ => _.Source, f => f.Lorem.Sentence(wordCount: 5));
@@ -21,7 +23,7 @@ namespace Logging.Data.Fakers
 			_faker.RuleFor(_ => _.HResult, f => f.Random.Int(-100, 100));
 		}
 
-		public static IEnumerable<InternalErrorLogEntryDbModel> Generate(int count = 10)
+		public static IEnumerable<InternalErrorLogEntryDbModel> Generate(int count = 100)
 			=> _faker.Generate(count);
 	}
 }
