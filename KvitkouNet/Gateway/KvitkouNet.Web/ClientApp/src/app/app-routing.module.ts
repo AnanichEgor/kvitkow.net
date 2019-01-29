0c21e2429450e31b7e4bf560e24fc3632ac1367e
Alex160881 <Alex160881@gmail.com>
2019-01-29 15:17:59 +0300

    *fix Front routing and movement all components to a folder "components"

--- KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/app-routing.module.ts
+++ KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AdminComponent } from './admin/admin.component';
-import { TicketFormComponent } from './ticket-form/ticket-form.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { TicketComponent } from './ticket/ticket.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { TicketFormComponent } from './components/ticket-form/ticket-form.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { UsersComponent } from './components/users/users.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'admin', component: AdminComponent, pathMatch: 'full' },
   { path: 'ticket/add', component: TicketFormComponent, pathMatch: 'full' },
+  { path: 'users', component: UsersComponent, pathMatch: 'full' },
+  { path: 'users/registration', component: RegistrationComponent, pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
   { path: 'ticket', component: TicketComponent, pathMatch: 'full' }
 ];
--- KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/app.module.ts
+++ KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/app.module.ts
@@ -5,15 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
-import { AdminComponent } from './admin/admin.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { TicketComponent } from './ticket/ticket.component';
-import { TicketFormComponent } from './ticket-form/ticket-form.component';
-import { UsersComponent } from './users/users.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { TicketFormComponent } from './components/ticket-form/ticket-form.component';
+import { UsersComponent } from './components/users/users.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
