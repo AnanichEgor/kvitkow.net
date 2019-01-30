4d798249a28a84499f019fa4a615e2a883979e08
Alexander Pashnikov <dr.pashnikov@google.com>
2019-01-31 01:13:26 +0300

    renamed enum type

--- KvitkouNet/Logging/Logging.Logic/Enums/TicketActionType.cs
+++ KvitkouNet/Logging/Logging.Logic/Enums/TicketActionType.cs
@@ -6,7 +6,7 @@ namespace Logging.Data.Enums
 	/// Перечисление, описывающее действия с билетом
 	/// </summary>
 	[Flags]
-	public enum TicketAction
+	public enum TicketActionType
 	{
 		/// <summary>
 		/// Неизвестное действие
