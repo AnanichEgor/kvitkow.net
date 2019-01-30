abdc693f3a2208ecd5e22fc81d1cd29ccf05f631
Andrei <agobrik92@gmail.com>
2019-01-30 18:37:45 +0300

    add Модель сообщения в чате, для пользователя который offline
    Необходимо доделать шаблон модели согласно тому, что нужно для натификации

---
+++ KvitkouNet/Gateway/KvitkouNet.Messages/Chat/OfflineChatMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvitkouNet.Messages.Chat
+{
+    /// <summary>
+    /// Модель сообщения в чате, для пользователя который offline
+    /// </summary>
+    public class OfflineChatMessage
+    {
+    }
+}
