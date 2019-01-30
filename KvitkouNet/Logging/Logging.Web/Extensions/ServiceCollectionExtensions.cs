b747470d9176f0ae97de98ff8828f8b461a3b002
Bakharev Mikhail <baharev96@list.ru>
2019-01-31 01:40:40 +0300

    * registered all consumers

--- KvitkouNet/Logging/Logging.Web/Extensions/ServiceCollectionExtensions.cs
+++ KvitkouNet/Logging/Logging.Web/Extensions/ServiceCollectionExtensions.cs
@@ -10,7 +10,12 @@ namespace Logging.Web.Extensions
 	{
 		public static IServiceCollection RegisterConsumers(this IServiceCollection services)
 		{
+			services.AddScoped<IConsumeAsync<AccountLogMessage>, AccountLogConsumer>();
 			services.AddScoped<IConsumeAsync<InternalErrorLogMessage>, InternalErrorLogConsumer>();
+			services.AddScoped<IConsumeAsync<PaymentLogMessage>, PaymentLogConsumer>();
+			services.AddScoped<IConsumeAsync<SearchQueryLogMessage>, SearchQueryLogConsumer>();
+			services.AddScoped<IConsumeAsync<TicketActionLogMessage>, TicketActionLogConsumer>();
+			services.AddScoped<IConsumeAsync<TicketDealLogMessage>, TicketDealLogConsumer>();
 			return services;
 		}
 	}
