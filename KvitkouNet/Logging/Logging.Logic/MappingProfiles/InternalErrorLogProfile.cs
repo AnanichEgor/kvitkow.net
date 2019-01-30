0d44d57ac00497d09307ba5b08883d6cb298832c
Bakharev Mikhail <baharev96@list.ru>
2019-01-31 00:47:24 +0300

    * renamed Exception  type property

--- KvitkouNet/Logging/Logging.Logic/MappingProfiles/InternalErrorLogProfile.cs
+++ KvitkouNet/Logging/Logging.Logic/MappingProfiles/InternalErrorLogProfile.cs
@@ -9,9 +9,9 @@ namespace Logging.Logic.MappingProfiles
 		public InternalErrorLogProfile()
 		{
 			CreateMap<InternalErrorLogEntry, InternalErrorLogEntryDbModel>()
-			    .ForMember(dbm => dbm.ExceptionType, opts => opts.MapFrom(m => m.TypeName))
+			    .ForMember(dbm => dbm.ExceptionType, opts => opts.MapFrom(m => m.ExceptionType))
 				.ReverseMap()
-			    .ForMember(m => m.TypeName, opts => opts.MapFrom(dbm => dbm.ExceptionType));
+			    .ForMember(m => m.ExceptionType, opts => opts.MapFrom(dbm => dbm.ExceptionType));
 		}
 	}
 }
