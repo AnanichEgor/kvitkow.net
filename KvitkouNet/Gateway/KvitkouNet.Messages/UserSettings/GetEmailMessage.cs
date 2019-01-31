2ad8bab1512c6bd954f03a1e3b1f47c141e7c8c4
Marina <for-marina@mail.ru>
2019-02-01 00:50:07 +0300

    * add usersettings messages

---
+++ KvitkouNet/Gateway/KvitkouNet.Messages/UserSettings/GetEmailMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvitkouNet.Messages.UserSettings
+{
+	public class GetEmailMessage
+	{
+		/// <summary>
+		/// Электронный адрес пользователя
+		/// </summary>
+		public string Email { get; set; }
+	}
+}
---
+++ KvitkouNet/Gateway/KvitkouNet.Messages/UserSettings/GetUserProfileMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvitkouNet.Messages.UserSettings
+{
+	public class GetUserProfileMessage
+	{
+		/// <summary>
+		/// Имя пользователя
+		/// </summary>
+		public string FirstName { get; set; }
+
+		/// <summary>
+		/// Отчество пользователя
+		/// </summary>
+		public string MiddleName { get; set; }
+
+		/// <summary>
+		/// Фамилия пользователя
+		/// </summary>
+		public string LastName { get; set; }
+
+		/// <summary>
+		/// Дата рождения
+		/// </summary>
+		public DateTime Birthday { get; set; }
+
+		/// <summary>
+		/// Настройки пользователя
+		/// </summary>
+		public string UserSettingsId { get; set; }
+	}
+}
