b2c4acb0311a84d9f4e88fce3aa9aa2c358e41e5
Bakharev Mikhail <baharev96@list.ru>
2019-02-02 23:05:03 +0300

    + added input form groups

--- KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/components/admin/error-logs/error-logs.component.ts
+++ KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/components/admin/error-logs/error-logs.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ErrorLogEntry } from '../../../models/errorLogEntry';
 import { LogService } from '../../../services/log.service';
+import {
+  FormControl,
+  Validators,
+  FormGroup,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'app-error-logs',
@@ -10,12 +16,22 @@ import { LogService } from '../../../services/log.service';
 export class ErrorLogsComponent implements OnInit {
   errorLogs : ErrorLogEntry[];
   errorLogTableHeaders = ['Id', 'Дата', 'Микросервис', 'Тип', 'Hresult', 'InnerEx', 'Сообщение', 'Источник', 'StackTrace', 'TargetSitename'];
+  errorLogsFormGroup: FormGroup;
+  
 
   constructor(
     private logService : LogService
   ) { }
 
   ngOnInit() {
+    this.errorLogsFormGroup = new FormGroup({
+         serviceName: new FormControl('serviceName'),
+         exceptionTypeName: new FormControl('exceptionTypeName'),
+         message: new FormControl('message')
+       })
+  }
+
+  onSubmit() {
     this.logService.getErrorLogs()
     .subscribe(result => this.errorLogs = result, err => console.error(err))
   }
