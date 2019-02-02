5da6063add2ac9038a14425e5a496f8584863a36
Alexander Pashnikov <dr.pashnikov@google.com>
2019-02-03 02:08:20 +0300

    moved errorloffiltervalidator to web

---
+++ KvitkouNet/Logging/Logging.Web/Validators/Filters/ErrorLogsFilterValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Logging.Logic.Models.Filters;
+
+namespace Logging.Web.Validators.Filters
+{
+	public class ErrorLogsFilterValidator : AbstractValidator<ErrorLogsFilter>
+	{
+		public ErrorLogsFilterValidator()
+		{
+		    CascadeMode = CascadeMode.StopOnFirstFailure;
+
+		    When(f => f.DateFrom.HasValue && f.DateTo.HasValue, () =>
+		    {
+		        RuleFor(f => f.DateFrom.Value)
+		            .LessThanOrEqualTo(f => f.DateTo.Value);
+		    });
+		}
+	}
+}
