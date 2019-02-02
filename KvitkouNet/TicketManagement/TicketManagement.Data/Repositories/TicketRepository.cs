0dcf3659bce74160590067ca9d2e6e84a61bafee
romaklayt <romakolyago18@gmail.com>
2019-02-02 19:43:36 +0300

    * now the ticket when attached is tied to the user

--- KvitkouNet/TicketManagement/TicketManagement.Data/Repositories/TicketRepository.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Data/Repositories/TicketRepository.cs
@@ -30,18 +30,28 @@ namespace TicketManagement.Data.Repositories
         /// <param name="ticket">Модель билета</param>
         /// <returns>Код ответа Create и добавленную модель</returns>
         public async Task<string> Add(Ticket ticket)
-        { 
+        {
             //WARNING используется для замены стандартных значений swagerr'a
             //(чтобы рукчками каждый раз не править)
             //при связи с фронтом надо убрать 
             ticket.Id = null;
             ticket.User.UserInfoId = null;
-            ticket.RespondedUsers[0].UserInfoId = null;
+            ticket.RespondedUsers = null;
             //WARNING
+            var user = await _context.UserInfos.Include(info => info.UserTickets)
+                .FirstOrDefaultAsync(info => info.UserInfoId == ticket.User.UserInfoId);
+            if (user == null)
+            {
+                _context.Tickets.Add(ticket);
+                await _context.SaveChangesAsync();
+                return _context.Tickets.Last()
+                    .Id;
+            }
 
-            _context.Tickets.Add(ticket);
+            user.UserTickets.Add(ticket);
             await _context.SaveChangesAsync();
-            return _context.Tickets.Last().Id;
+            return _context.Tickets.Last()
+                .Id;
         }
 
         /// <summary>
