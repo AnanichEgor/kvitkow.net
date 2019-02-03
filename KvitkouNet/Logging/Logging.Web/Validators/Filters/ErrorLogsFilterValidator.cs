56938cf2e3ef25efa9dc682b4e636592cf1b10d4
Alexander Pashnikov <dr.pashnikov@google.com>
2019-02-03 17:24:38 +0300

    implemented validators for particular log filters

--- KvitkouNet/Logging/Logging.Web/Validators/Filters/ErrorLogsFilterValidator.cs
+++ KvitkouNet/Logging/Logging.Web/Validators/Filters/ErrorLogsFilterValidator.cs
@@ -1,19 +1,8 @@
-﻿using FluentValidation;
-using Logging.Logic.Models.Filters;
+﻿using Logging.Logic.Models.Filters;
 
 namespace Logging.Web.Validators.Filters
 {
-	public class ErrorLogsFilterValidator : AbstractValidator<ErrorLogsFilter>
+	public class ErrorLogsFilterValidator : BaseFilterValidator<ErrorLogsFilter>
 	{
-		public ErrorLogsFilterValidator()
-		{
-		    CascadeMode = CascadeMode.StopOnFirstFailure;
-
-		    When(f => f.DateFrom.HasValue && f.DateTo.HasValue, () =>
-		    {
-		        RuleFor(f => f.DateFrom.Value)
-		            .LessThanOrEqualTo(f => f.DateTo.Value);
-		    });
-		}
 	}
 }
---
+++ KvitkouNet/Logging/Logging.Web/Validators/Filters/PaymentLogsFilterValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Logging.Logic.Models.Filters;
+
+namespace Logging.Web.Validators.Filters
+{
+    public class PaymentLogsFilterValidator : BaseFilterValidator<PaymentLogsFilter>
+    {
+        public PaymentLogsFilterValidator()
+        {
+            When(f => f.MinTransfer.HasValue, () =>
+            {
+                RuleFor(f => f.MinTransfer.Value)
+                    .GreaterThanOrEqualTo(0);
+            });
+
+            When(f => f.MinTransfer.HasValue && f.MaxTransfer.HasValue, () =>
+            {
+                RuleFor(f => f.MinTransfer.Value)
+                    .LessThanOrEqualTo(f => f.MaxTransfer.Value);
+            });
+        }
+    }
+}
