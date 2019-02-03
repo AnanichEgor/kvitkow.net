83f9a89a9b77c6eac1558460cfec59761779d3d8
Bakharev Mikhail <baharev96@list.ru>
2019-02-03 17:36:33 +0300

    + added account logs page in angular client

--- KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/components/admin/account-logs/account-logs.component.ts
+++ KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/components/admin/account-logs/account-logs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LogService } from '../../../services/log.service';
 import { AccountLogEntry } from '../../../models/accountLogEntry';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-account-logs',
@@ -15,6 +15,29 @@ export class AccountLogsComponent implements OnInit {
   constructor(private logService: LogService) { }
 
   ngOnInit() {
+    this.accountLogsFormGroup = new FormGroup({
+      dateFrom: new FormControl('2018-12-12'),
+      dateTo: new FormControl('2018-12-13'),
+      userId: new FormControl('userId'),
+      userName: new FormControl('userName'),
+      email: new FormControl('email'),
+      type: new FormControl(0)
+    })
   }
 
+  onSubmit() {
+    this.logService.getAccountLogs(this.getParamsString())
+      .subscribe(result => this.accountLogs = result, err => console.error(err))
+  }
+
+  getParamsString()  {
+    const params = new URLSearchParams();
+    const formValue = this.accountLogsFormGroup.value;
+
+    for (const key in formValue) {
+      params.append(key, formValue[key]);
+    }
+    console.log(params.toString());
+    return params.toString();
+  }
 }
