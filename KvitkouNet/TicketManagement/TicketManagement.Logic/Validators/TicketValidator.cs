8412c7b10336bc531f7a3d657291c8343d264f7e
romaklayt <romakolyago18@gmail.com>
2019-02-05 01:17:57 +0300

    - del default check user

--- KvitkouNet/TicketManagement/TicketManagement.Logic/Validators/TicketValidator.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Validators/TicketValidator.cs
@@ -13,7 +13,6 @@ namespace TicketManagement.Logic.Validators
             RuleFor(ticket => ticket.Name).NotEmpty().Length(5, 100);
             RuleFor(ticket => ticket.AdditionalData).MaximumLength(240);
             RuleFor(ticket => ticket.SellerPhone).NotEmpty().Length(6, 20).Matches(@"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$");
-            RuleFor(ticket => ticket.User).SetValidator(new UserValidator());
             RuleFor(ticket => ticket.LocationEvent).SetValidator(new AddressValidator());
             RuleFor(ticket => ticket.SellerAdress).SetValidator(new AddressValidator());
             RuleFor(ticket => ticket.TypeEvent).IsInEnum();
