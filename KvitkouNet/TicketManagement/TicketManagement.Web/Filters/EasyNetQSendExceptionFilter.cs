e8f3d1e9a54396e3c2ab740516f007bb53bb9f82
romaklayt <romakolyago18@gmail.com>
2019-02-06 01:18:36 +0300

    + add EasyNetQSendExceptionFilter

---
+++ KvitkouNet/TicketManagement/TicketManagement.Web/Filters/EasyNetQSendExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TicketManagement.Logic.Exceptions;
+
+namespace TicketManagement.Web.Filters
+{
+    public class EasyNetQSendExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is EasyNetQSendException)
+            {
+                var res =(EasyNetQSendException) context.Exception;
+                if (res.resultResponse != null)
+                {
+                    context.Result = new OkObjectResult(res.resultResponse);
+                }
+                else
+                {
+                    context.Result = new NoContentResult();
+                }
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
