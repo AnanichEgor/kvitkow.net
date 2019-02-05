15a1c75d01643b613174137cabe84e068d02f6c1
romaklayt <romakolyago18@gmail.com>
2019-02-06 01:18:51 +0300

    + add UserExceptionFilter

---
+++ KvitkouNet/TicketManagement/TicketManagement.Web/Filters/UserExceptionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TicketManagement.Logic.Exceptions;
+
+namespace TicketManagement.Web.Filters
+{
+    public class UserExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is UserException)
+            {
+                context.Result = new BadRequestObjectResult(context.Exception.Message);
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
