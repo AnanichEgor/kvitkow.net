759c9a15c28df70d8ce5c5b198b5cd53de3ded54
romaklayt <romakolyago18@gmail.com>
2019-02-06 01:18:14 +0300

    * fixed error

--- KvitkouNet/TicketManagement/TicketManagement.Logic/Exceptions/EasyNetQSendException.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Exceptions/EasyNetQSendException.cs
@@ -3,11 +3,11 @@ using System;
 
 namespace TicketManagement.Logic.Exceptions
 {
-    class EasyNetQSendException : EasyNetQException
+   public class EasyNetQSendException : EasyNetQException
     {
         public EasyNetQSendException(string message, Exception exception) : base(message, exception)
         {
-            this.resultResponse = resultResponse;
+           
         }
         public EasyNetQSendException(string message, Exception exception, string resultResponse) : base(message, exception)
         {
@@ -15,6 +15,6 @@ namespace TicketManagement.Logic.Exceptions
         }
 
         public string resultResponse { get; private set; }
-
+        
     }
 }
