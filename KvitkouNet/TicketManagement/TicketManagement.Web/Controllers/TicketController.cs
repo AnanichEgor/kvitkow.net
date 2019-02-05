f15dd2add998c83db4758c3665692f2f68adcd15
romaklayt <romakolyago18@gmail.com>
2019-02-05 14:06:08 +0300

    * edit get value in Delete

--- KvitkouNet/TicketManagement/TicketManagement.Web/Controllers/TicketController.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Web/Controllers/TicketController.cs
@@ -89,7 +89,7 @@ namespace TicketManagement.Web.Controllers
         [SwaggerResponse(HttpStatusCode.BadRequest, typeof(string), Description = "Error")]
         public async Task<IActionResult> DeleteAll()
         {
-            var result = await _service.DeleteAll();
+            ResponseModel responseInfo = await _service.DeleteAll();
             return NoContent();
         }
 
@@ -105,7 +105,7 @@ namespace TicketManagement.Web.Controllers
         [SwaggerResponse(HttpStatusCode.BadRequest, typeof(string), Description = "Error")]
         public async Task<IActionResult> Delete([FromRoute] string id)
         {
-            var result = await _service.Delete(id);
+            ResponseModel responseInfo = await _service.Delete(id);
             return NoContent();
         }
 
