e8d659f361d98eae17c2aa551bc6ffce53137c27
romaklayt <romakolyago18@gmail.com>
2019-02-05 12:53:40 +0300

    + add response model

---
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Models/ResponseModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation.Results;
+using TicketManagement.Logic.Models.Enums;
+
+namespace TicketManagement.Logic.Models
+{
+    /// <summary>
+    /// Модель статуса ответа
+    /// </summary>
+    public class ResponseModel
+    {
+        /// <summary>
+        /// Успешность запроса
+        /// </summary>
+        public RequestStatus Status { get; set; } = RequestStatus.Success;
+        /// <summary>
+        /// Доп. сообщение к статусу
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// Критическая ошибка
+        /// </summary>
+        public Exception Exception { get; set; }
+        /// <summary>
+        /// List ошибок валидации
+        /// </summary>
+        public List<ValidationFailure> ValidationFailures { get; set; }
+    }
+}
