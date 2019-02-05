873ea97e99f64248d1b5402b24b20a16793448f7
romaklayt <romakolyago18@gmail.com>
2019-02-06 02:05:23 +0300

    + add TicketNotFoundExceptionFilter

---
+++ KvitkouNet/TicketManagement/TicketManagement.Web/Filters/TicketNotFoundExceptionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TicketManagement.Data.Exceptions;
+
+namespace TicketManagement.Web.Filters
+{
+    public class TicketNotFoundExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is TicketNotFoundException)
+            {
+                context.Result = new BadRequestObjectResult(context.Exception.Message);
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
