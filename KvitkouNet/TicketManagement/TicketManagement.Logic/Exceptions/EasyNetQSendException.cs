d80cfca609a271500f402d96996f500a16e08495
romaklayt <romakolyago18@gmail.com>
2019-02-06 00:27:09 +0300

    + add custom EasyNetQSendException

---
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Exceptions/EasyNetQSendException.cs
@@ -0,0 +1,20 @@
+﻿using EasyNetQ;
+using System;
+
+namespace TicketManagement.Logic.Exceptions
+{
+    class EasyNetQSendException : EasyNetQException
+    {
+        public EasyNetQSendException(string message, Exception exception) : base(message, exception)
+        {
+            this.resultResponse = resultResponse;
+        }
+        public EasyNetQSendException(string message, Exception exception, string resultResponse) : base(message, exception)
+        {
+            this.resultResponse = resultResponse;
+        }
+
+        public string resultResponse { get; private set; }
+
+    }
+}
