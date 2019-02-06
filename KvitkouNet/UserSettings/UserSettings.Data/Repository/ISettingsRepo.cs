96ba68c0c91b83bab648c81aca8209ed99edae8e
Marina <for-marina@mail.ru>
2019-02-06 13:32:16 +0300

    +add model

--- KvitkouNet/UserSettings/UserSettings.Data/Repository/ISettingsRepo.cs
+++ KvitkouNet/UserSettings/UserSettings.Data/Repository/ISettingsRepo.cs
@@ -10,11 +10,37 @@ namespace UserSettings.Data
 	{
 		Task<SettingsDb> Get(string id);
 
+		/// <summary>
+		/// Обновление информации о том какие уведомления получать 
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="notifications"></param>
+		/// <returns></returns>
 		Task<bool> UpdateNotifications(string id, NotificationDb notifications);
 
+		/// <summary>
+		/// Обновление предпочтений по отображению информации
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="address"></param>
+		/// <param name="region"></param>
+		/// <param name="place"></param>
+		/// <returns></returns>
 		Task<bool> UpdatePreferences(string id, string address, string region, string place);
 
+		/// <summary>
+		/// Удаление аккаунта
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
 		Task DeleteAccount(string id);
+
+		/// <summary>
+		/// Обновление информации о том какая информация о пользователе доступна для при просмотре профиля
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="visibleInfoDb"></param>
+		/// <returns></returns>
 		Task<bool> UpdateVisible(string id, VisibleInfoDb visibleInfoDb);
 	}
 }
