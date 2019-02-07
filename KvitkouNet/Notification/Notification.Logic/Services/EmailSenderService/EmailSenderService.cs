713d178d8b11d005954a9f0404ebbb82df73ed97
artemGlotov <artsiom.hlotau@gmail.com>
2019-02-07 03:53:14 +0300

    update notificationService

--- KvitkouNet/Notification/Notification.Logic/Services/EmailSenderService/EmailSenderService.cs
+++ KvitkouNet/Notification/Notification.Logic/Services/EmailSenderService/EmailSenderService.cs
@@ -2,16 +2,17 @@
 using MimeKit;
 using MailKit.Net.Smtp;
 using Notification.Logic.Models.Requests;
+using Notification.Logic.Configs;
 
 namespace Notification.Logic.Services.EmailSenderService
 {
 	public class EmailSenderService : IEmailSenderService
 	{
-		public async Task SendEmailAsync(SendEmailRequest request)
+		public async Task SendEmailAsync(SendEmailRequest request, SenderConfig senderConfig)
 		{
 			MimeMessage emailMessage = new MimeMessage();
 
-			emailMessage.From.Add(new MailboxAddress(request.SenderName, request.SenderEmail));
+			emailMessage.From.Add(new MailboxAddress(senderConfig.Name, senderConfig.Email));
 			emailMessage.To.Add(new MailboxAddress(request.ReceiverName, request.ReceiverEmail));
 			emailMessage.Subject = request.Subject;
 
@@ -25,7 +26,7 @@ namespace Notification.Logic.Services.EmailSenderService
 				try
 				{
 					await client.ConnectAsync("smtp.gmail.com", 587, false);
-					await client.AuthenticateAsync(request.SenderEmail, request.SenderPassword);
+					await client.AuthenticateAsync(senderConfig.Email, senderConfig.Password);
 					await client.SendAsync(emailMessage);
 					await client.DisconnectAsync(true);
 				}
--- KvitkouNet/Notification/Notification.Logic/Services/NotificationService/NotificationService.cs
+++ KvitkouNet/Notification/Notification.Logic/Services/NotificationService/NotificationService.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
 using Notification.Data.Context;
 using Notification.Logic.Models;
 using Notification.Logic.Models.Requests;
+using Notification.Data.Models;
+using Notification.Data.Models.Enums;
 
 namespace Notification.Logic.Services.NotificationService
 {
@@ -23,39 +24,68 @@ namespace Notification.Logic.Services.NotificationService
 		}
 
 		public async Task AddUserNotifications(UserNotificationBulkRequest request)
-		{			
-			throw new NotImplementedException();
+		{
+			IQueryable<User> users = m_context.Users.AsNoTracking().Where(x => request.UserIds.Any(id => id == x.Id));
+			Data.Models.Notification notification = m_mapper.Map<NotificationMessage, Data.Models.Notification>(request.Message);
+			DateTime date = DateTime.UtcNow;
+			m_context.ChangeTracker.AutoDetectChangesEnabled = false;
+			foreach (User user in users)
+			{
+				m_context.Notifications.Add(new Data.Models.Notification
+				{
+					User = user,
+					Date = date,
+					IsClosed = false,
+					Type = NotificationType.Notification,
+					Text = notification.Text,
+					Title = notification.Title,
+					Severity = notification.Severity
+				});
+			}
+			await m_context.SaveChangesAsync();
 		}
 
 		public async Task EditNotification(UserNotification userNotification)
 		{
-			throw new NotImplementedException();
+			Data.Models.Notification oldNotification = await m_context.Notifications.SingleOrDefaultAsync(x => x.Id == userNotification.NotificationId);
+			m_mapper.Map(userNotification, oldNotification);
+
+			//m_context.Attach(notification);
+			//m_context.Entry(notification).State = EntityState.Modified;
+			await m_context.SaveChangesAsync();
 		}
 
 		// проверочный метод
 		public async Task<IEnumerable<UserNotification>> GetAll()
 		{
-			var result = m_context.Notifications.AsNoTracking()
+			Data.Models.Notification[] result = m_context.Notifications.AsNoTracking()
 				.Include(x => x.User).ToArray();
 
-			var mapped = m_mapper.Map<UserNotification[]>(result);
+            UserNotification[] mapped = m_mapper.Map<UserNotification[]>(result);
 
 			return await Task.FromResult(mapped.AsEnumerable());
 		}
 
 		public async Task<UserNotification> GetNotification(string notificationId)
 		{
-			throw new NotImplementedException();
+			return m_mapper.Map<UserNotification>(await m_context.Notifications.AsNoTracking().FirstAsync(x => x.Id == notificationId));
 		}
 
 		public async Task<IEnumerable<UserNotification>> GetUserNotifications(string userId, bool onlyOpen)
 		{
-			throw new NotImplementedException();
+			Data.Models.Notification[] result = m_context.Notifications.AsNoTracking()
+				.Include(x => x.User).Where(x => x.User.Id == userId && x.IsClosed != onlyOpen).ToArray();
+
+			UserNotification[] mapped = m_mapper.Map<UserNotification[]>(result);
+
+			return await Task.FromResult(mapped.AsEnumerable());
 		}
 
 		public async Task SetStatusClosed(string notificationId)
 		{
-			throw new NotImplementedException();
+			Data.Models.Notification notification = await m_context.Notifications.SingleOrDefaultAsync(x => x.Id == notificationId);
+			notification.IsClosed = true;
+			await m_context.SaveChangesAsync();
 		}
 
 		public void Dispose()
