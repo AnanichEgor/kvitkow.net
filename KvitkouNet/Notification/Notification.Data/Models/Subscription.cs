0959f2f7af40416c7576333c86739cdfb39e9e85
artemGlotov <artsiom.hlotau@gmail.com>
2019-02-07 03:44:16 +0300

    add UserSubscriptionMessage

---
+++ KvitkouNet/Notification/Notification.Data/Models/Subscription.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notification.Data.Models
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Subscription
+	{
+		/// <summary>
+		/// Id подписки
+		/// </summary>
+		public string Id { get; set; }
+
+		/// <summary>
+		/// Тема
+		/// </summary>
+		public string Theme { get; set; }
+
+
+		/// <summary>
+		/// Подписки для пользователя
+		/// </summary>
+		public ICollection<UserSubscription> UserSubscriptions { get; set; }
+	}
+}
---
+++ KvitkouNet/Notification/Notification.Logic/MappingProfiles/SubscriptionProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+namespace Notification.Logic.MappingProfiles
+{
+	public class SubscriptionProfile : Profile
+	{
+		public SubscriptionProfile()
+		{
+            CreateMap<Models.Subscription, Data.Models.Subscription>()
+                .ForMember(data => data.Id,
+                    opt => opt.MapFrom(logic => logic.Id))
+                .ForPath(data => data.Theme,
+                    opt => opt.MapFrom(logic => logic.Theme))
+                .ReverseMap()
+                .ForMember(logic => logic.Id,
+                    opt => opt.MapFrom(data => data.Id))
+                .ForPath(logic => logic.Theme,
+                    opt => opt.MapFrom(data => data.Theme));
+		}
+	}
+}
