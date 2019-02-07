a99adbfb3b21e44f3617decb67a1d49007a4fd82
artemGlotov <artsiom.hlotau@gmail.com>
2019-02-07 03:46:07 +0300

    add subscriptionService and implementation

---
+++ KvitkouNet/Notification/Notification.Logic/Models/Requests/SubscriptionRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notification.Logic.Models.Requests
+{
+    /// <summary>
+    /// Запрос на подписку пользователя
+    /// </summary>
+    public class SubscriptionRequest
+    {
+        /// <summary>
+        /// Ид пользователя
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Тема
+        /// </summary>
+        /// <remarks>Если темы не существует, то добавится</remarks>
+        public string Theme { get; set; }
+
+        /// <summary>
+        /// Необходимо уведомление по почте
+        /// </summary>
+        public bool EmailNotificationNeeded { get; set; }
+
+        /// <summary>
+        /// Необходимо уведомление на клиенте 
+        /// </summary>
+        public bool ClientNotificationNeeded { get; set; }
+    }
+}
