ed0d80e9f8f798a793c167431af4e697afdb6879
romaklayt <romakolyago18@gmail.com>
2019-02-09 00:49:15 +0300

    + add UserDeleteMessageConsumerFromSettings

---
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Subscriber/UserDeleteMessageConsumerFromSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using EasyNetQ.AutoSubscribe;
+using KvitkouNet.Messages.UserManagement;
+using KvitkouNet.Messages.UserSettings;
+using TicketManagement.Data.Context;
+using TicketManagement.Data.DbModels;
+
+namespace TicketManagement.Logic.Subscriber
+{
+  public  class UserDeleteMessageConsumerFromSettings:IConsumeAsync<DeleteUserProfileMessage>
+    {
+        private readonly IMapper _mapper;
+        private readonly TicketContext _ticketContext;
+
+        public UserDeleteMessageConsumerFromSettings(TicketContext ticketContext, IMapper mapper)
+        {
+            _ticketContext = ticketContext;
+            _mapper = mapper;
+        }
+
+        public async Task ConsumeAsync(DeleteUserProfileMessage message)
+        {
+            var modelDb = _mapper.Map<UserInfo>(message);
+            _ticketContext.UserInfos.Remove(modelDb);
+            await _ticketContext.SaveChangesAsync();
+        }
+    }
+}
