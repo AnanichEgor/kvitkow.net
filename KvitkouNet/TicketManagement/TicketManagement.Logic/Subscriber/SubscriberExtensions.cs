dfc18634c00b8b5b24255a04b9c694b8873846d1
romaklayt <romakolyago18@gmail.com>
2019-02-09 15:33:05 +0300

    * edit SubscriberExtensions

--- KvitkouNet/TicketManagement/TicketManagement.Logic/Subscriber/SubscriberExtensions.cs
+++ KvitkouNet/TicketManagement/TicketManagement.Logic/Subscriber/SubscriberExtensions.cs
@@ -1,10 +1,12 @@
-﻿using EasyNetQ;
+﻿using System;
+using EasyNetQ;
 using EasyNetQ.AutoSubscribe;
 using KvitkouNet.Messages.UserManagement;
 using KvitkouNet.Messages.UserSettings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
 
 namespace TicketManagement.Logic.Subscriber
 {
@@ -12,27 +14,51 @@ namespace TicketManagement.Logic.Subscriber
     {
         public static IApplicationBuilder UseSubscriber(this IApplicationBuilder app)
         {
+            var userUpdatedPreffix = "UserForTicket.Updated";
+            var userSetUpdatedPreffix = "UserSettingsForTicket.Updated";
+            var userDeletedPreffix = "UserForTicket.Deleted";
+            var userSetDeletedPreffix = "UserSettingsForTicket.Deleted";
             var services = app.ApplicationServices.CreateScope().ServiceProvider;
-
+            var policy = Policy.Handle<TimeoutException>()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1)
+                });
             var lifetime = services.GetService<IApplicationLifetime>();
             var bus = services.GetService<IBus>();
-
-            lifetime.ApplicationStarted.Register(() =>
+            
+            lifetime.ApplicationStarted.Register(async () =>
             {
-                var userUpdatedPreffix = "UserForTicket.Updated";
-                var userSetUpdatedPreffix = "UserSettingsForTicket.Updated";
-                var userDeletedPreffix = "UserForTicket.Deleted";
-                var userSetDeletedPreffix = "UserSettingsForTicket.Deleted";
-                bus.SubscribeAsync<UserDeletedMessage>(userDeletedPreffix,
-                    msg => services.GetService<IConsumeAsync<UserDeletedMessage>>().ConsumeAsync(msg));
-                bus.SubscribeAsync<UserUpdatedMessage>(userUpdatedPreffix,
-                    msg => services.GetService<IConsumeAsync<UserUpdatedMessage>>().ConsumeAsync(msg));
-                bus.SubscribeAsync<UserProfileUpdateMessage>(userSetUpdatedPreffix,
-                    msg => services.GetService<IConsumeAsync<UserProfileUpdateMessage>>().ConsumeAsync(msg));
-                bus.SubscribeAsync<DeleteUserProfileMessage>(userSetDeletedPreffix,
-                    msg => services.GetService<IConsumeAsync<DeleteUserProfileMessage>>().ConsumeAsync(msg));
-            });
+                try
+                {
+                    await policy.ExecuteAsync(async () =>
+                    {
+                        bus.SubscribeAsync<UserDeletedMessage>(userDeletedPreffix,
+                            msg => services.GetService<IConsumeAsync<UserDeletedMessage>>().ConsumeAsync(msg));
+                    });
 
+                    await policy.ExecuteAsync(async () =>
+                    {
+                        bus.SubscribeAsync<UserUpdatedMessage>(userUpdatedPreffix,
+                            msg => services.GetService<IConsumeAsync<UserUpdatedMessage>>().ConsumeAsync(msg));
+                    });
+                    await policy.ExecuteAsync(async () =>
+                    {
+                        bus.SubscribeAsync<UserProfileUpdateMessage>(userSetUpdatedPreffix,
+                            msg => services.GetService<IConsumeAsync<UserProfileUpdateMessage>>().ConsumeAsync(msg));
+                    });
+                    await policy.ExecuteAsync(async () =>
+                    {
+                        bus.SubscribeAsync<DeleteUserProfileMessage>(userSetDeletedPreffix,
+                            msg => services.GetService<IConsumeAsync<DeleteUserProfileMessage>>().ConsumeAsync(msg));
+                    });
+                }
+                catch (TimeoutException e)
+                {
+					//used to bypass RabbitMq subscription error
+                }
+            });
+            
             lifetime.ApplicationStopped.Register(() => bus.Dispose());
 
             return app;
