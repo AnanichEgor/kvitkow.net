3dc0038d0c389fb70c24a822fe59f7413119f48d
romaklayt <romakolyago18@gmail.com>
2019-02-10 00:48:27 +0300

    - del enums front

--- KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/models/ticketStatus.ts
+++
@@ -1,8 +0,0 @@
-export type TicketStatus = 0 | 1 | 2 | 3;
-
-export const TicketStatus = {
-    NUMBER_0: 0 as TicketStatus,
-    NUMBER_1: 1 as TicketStatus,
-    NUMBER_2: 2 as TicketStatus,
-    NUMBER_3: 3 as TicketStatus
-};
