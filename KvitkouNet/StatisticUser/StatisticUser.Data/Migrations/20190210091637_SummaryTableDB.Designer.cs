0e3e4778e2e2ad92f87f60e6983796d9a9216ffb
Egor Ananich <ev.ananich@gmail.com>
2019-02-10 12:16:59 +0300

    + add SummaryTableDB

--- KvitkouNet/StatisticUser/StatisticUser.Data/Migrations/20190210091637_SummaryTableDB.Designer.cs
+++ KvitkouNet/StatisticUser/StatisticUser.Data/Migrations/20190210091637_SummaryTableDB.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StatisticUser.Data;
 
 namespace StatisticUser.Data.Migrations
 {
     [DbContext(typeof(WebApiContext))]
-    partial class WebApiContextModelSnapshot : ModelSnapshot
+    [Migration("20190210091637_SummaryTableDB")]
+    partial class SummaryTableDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
