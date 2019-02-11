b4612de3e3967136b7f95aac6d8bba893f8b86f3
romaklayt <romakolyago18@gmail.com>
2019-02-11 21:44:57 +0300

    + add OAuthService in AddTicketService

--- KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/services/add-ticket.service.ts
+++ KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/services/add-ticket.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Ticket } from '../models/ticket';
-
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddTicketService {
-
   private baseUrl = 'http://localhost:5007';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private oauthService: OAuthService) {}
 
-    sendTicket(body){
-      return this.http.post(`${this.baseUrl}/api/tickets`, body);
+  sendTicket(body) {
+    return this.http.post(
+      `${this.baseUrl}/api/tickets`,
+      { headers: this.getHeaders() },
+      body
+    );
+  }
+  isAuthenticated() {
+    const token = this.oauthService.getAccessToken();
+    return !!token ? true : false;
+  }
+  private getHeaders() {
+    const token = this.oauthService.getAccessToken();
+    return !!token
+      ? new HttpHeaders({
+          Authorization: 'Bearer ' + token
+        })
+      : new HttpHeaders();
   }
 }
