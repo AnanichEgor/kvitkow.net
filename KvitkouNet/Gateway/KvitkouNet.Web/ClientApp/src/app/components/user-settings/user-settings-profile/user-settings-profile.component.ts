63e0162dc5328e4d11f2c8648342a28e4a39326f
Marina <for-marina@mail.ru>
2019-02-14 17:17:13 +0300

    * add token

--- KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/components/user-settings/user-settings-profile/user-settings-profile.component.ts
+++ KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/components/user-settings/user-settings-profile/user-settings-profile.component.ts
@@ -1,6 +1,10 @@
+import { OAuthService } from 'angular-oauth2-oidc';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Users } from 'src/app/models/users';
+import { UpdateFioService } from 'src/app/services/user-settings/update-fio.service';
+import { ForUpdateModel } from 'src/app/models/users/forUpdateModel';
+import * as jwt_decode from "jwt-decode";
 
 @Component({
   selector: 'app-user-settings-profile',
@@ -9,14 +13,42 @@ import { Users } from 'src/app/models/users';
 })
 export class UserSettingsProfileComponent implements OnInit {
   user: Users
+  model: ForUpdateModel
+  id: string = this.getUserId()
   userSettingsProfile = new FormGroup({
     first: new FormControl(''),
     middle: new FormControl(''),
     last: new FormControl('')
   });
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private updateFioService: UpdateFioService, private oauthService: OAuthService) { 
+    this.id = this.getUserId();
   }
 
+  ngOnInit() {
+    
+    console.log(this.id)
+    this.updateFioService.getProfile(this.id).subscribe(result=>(this.user = result), err => console.log(err))
+  }
+  onPut(){
+    this.model.firstName = this.user.firstName;
+    this.model.lastName = this.user.firstName;
+    this.model.birthday = this.user.birthday;
+    this.updateFioService.putProfile(this.id, this.model).subscribe(err => console.log(err));
+  }
+  getUserId(): string {
+    var decodedToken = this.getDecodedAccessToken(this.oauthService.getAccessToken());
+    
+    var result = decodedToken == null ? 11 : decodedToken['id'];
+
+    return result == null ? 11 : result;
+
+    }
+  getDecodedAccessToken(token: string): any {
+     try{
+        return jwt_decode(token);
+    }
+     catch(Error){
+         return null;
+     }
+  }
 }
--- KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/services/user-settings/update-fio.service.ts
+++ KvitkouNet/Gateway/KvitkouNet.Web/ClientApp/src/app/services/user-settings/update-fio.service.ts
@@ -8,9 +8,11 @@ import { Users } from 'src/app/models/users';
 export class UpdateFioService {
   private baseUrl = 'http://localhost:5003';
   constructor(private http: HttpClient) { }
+
   getProfile(id) {
     return this.http.get<Users>(`${this.baseUrl}/api/users/${id}`);
   }
+  
   putProfile(id, body) {
     return this.http.put(`${this.baseUrl}/api/users/${id}`, body)
   }
